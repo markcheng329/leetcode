0546077d8274ac7d00e449710d5b3b0b6c2f08cd
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-12 04:58:52 -0500

    Create 146-LruCache

---
+++ 146-LruCache/146-LruCache.py
@@ -0,0 +1,48 @@
+# Last updated: 11/12/2025, 4:58:51 AM
+class Node:
+    def __init__(self,key,value):
+        self.key,self.value = key,value
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+        self.left,self.right = Node(0,0),Node(0,0)
+        self.left.next,self.right.prev = self.right,self.left
+
+    def remove(self,node):
+        prev,nxt = node.prev,node.next
+        prev.next,nxt.prev = nxt,prev
+
+    def add(self,node):
+        prev,nxt = self.right.prev,self.right
+        prev.next = nxt.prev = node
+        node.prev,node.next = prev,nxt        
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.add(self.cache[key])
+            return self.cache[key].value
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key,value)
+        self.add(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
