72dc85621a5e8c3a6281fe45ccfd6fead0802d69
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-12 04:57:00 -0500

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.py
@@ -0,0 +1,16 @@
+# Last updated: 11/12/2025, 4:56:59 AM
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i = j = 0
+
+        res = []
+
+        while i < len(word1) or j<len(word2):
+            if i < len(word1):
+                res.append(word1[i])
+                i+=1
+            if j < len(word2):
+                res.append(word2[j])
+                j+=1
+        
+        return "".join(res)
