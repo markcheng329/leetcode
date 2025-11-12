7b426faaf4d39aec2919d0c674fdd16bb3a01544
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-12 04:57:10 -0500

    Create 1036-RottingOranges

---
+++ 1036-RottingOranges/1036-RottingOranges.py
@@ -0,0 +1,27 @@
+# Last updated: 11/12/2025, 4:57:10 AM
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows,cols = len(grid),len(grid[0])
+        res = 0
+        fresh = 0
+        q = deque()
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    fresh +=1
+                if grid[r][c] ==2:
+                    q.append((r,c))
+        
+        while fresh > 0 and q:
+            for i in range(len(q)):
+                r,c = q.popleft()
+                for dr,dc in [1,0],[-1,0],[0,1],[0,-1]:
+                    nr,nc = r+dr,c+dc
+                    if (nr in range(rows) and nc in range(cols) and grid[nr][nc] == 1):
+                        grid[nr][nc] = 2
+                        q.append((nr,nc))
+                        fresh -=1
+            res +=1
+                     
+        return res if fresh == 0 else -1
