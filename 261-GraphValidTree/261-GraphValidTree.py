090ad7e513da1fb88d0a82d852cbcdc82b059b28
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-12 04:57:57 -0500

    Create 261-GraphValidTree

---
+++ 261-GraphValidTree/261-GraphValidTree.py
@@ -0,0 +1,29 @@
+# Last updated: 11/12/2025, 4:57:56 AM
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > n-1:
+            return False
+        
+        adj = [[] for i in range(n)]
+
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        visit = set()
+        
+        def dfs(node,parent):
+            if node in visit:
+                return False
+            
+            visit.add(node)
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                
+                if not dfs(nei,node):
+                    return False
+            return True
+
+        return True if dfs(0,-1) and len(visit) ==n else False
+
