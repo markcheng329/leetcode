6108d746486f88e03d3ab5ac4c1ca37ee678574a
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-13 05:35:24 -0500

    File updated at 11/13/2025, 5:35:23 AM

--- 90-SubsetsII/90-SubsetsII.py
+++ 90-SubsetsII/90-SubsetsII.py
@@ -1,22 +1,20 @@
-# Last updated: 11/13/2025, 5:31:55 AM
+# Last updated: 11/13/2025, 5:35:23 AM
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()          
+        nums.sort()
         res = []
         subset = []
 
-        def backtrack(i):
+        def backtracking(i):
             res.append(subset.copy())
-            for j in range(i, len(nums)):
-                if j > i and nums[j] == nums[j - 1]:
+
+            for j in range(i,len(nums)):
+                if j > i and nums[j] == nums[j-1]:
                     continue
-
-                # 选择 nums[i]
+            
                 subset.append(nums[j])
-                # 继续往后选，下一层从 i+1 开始
-                backtrack(j + 1)
-                # 撤销选择
+                backtracking(j+1)
                 subset.pop()
-
-        backtrack(0)
+        
+        backtracking(0)
         return res
