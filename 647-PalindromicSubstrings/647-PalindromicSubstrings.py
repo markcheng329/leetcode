8a02f5b0b2be71c9b872feb19c934172125acd3e
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-13 04:49:41 -0500

    File updated at 11/13/2025, 4:49:40 AM

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.py
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.py
@@ -1,4 +1,4 @@
-# Last updated: 11/12/2025, 4:57:28 AM
+# Last updated: 11/13/2025, 4:49:40 AM
 class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
@@ -9,12 +9,12 @@ class Solution:
                 res +=1
                 l -=1
                 r +=1
-
+            
             l,r = i,i+1
             while l >= 0 and r< len(s) and s[l] == s[r]:
                 res +=1
                 l -=1
                 r +=1
+            
         return res
-
                 
