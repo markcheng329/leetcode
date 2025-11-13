2fefd2f0750948a01eef9ac1f2ff182dbb6d86d1
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-13 02:12:45 -0500

    File updated at 11/13/2025, 2:12:45 AM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -1,4 +1,4 @@
-# Last updated: 7/19/2025, 11:19:56 PM
+# Last updated: 11/13/2025, 2:12:45 AM
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
@@ -8,13 +8,12 @@ class Solution:
         directions = [1,0],[-1,0],[0,1],[0,-1]
 
         def dfs(r,c):
-            if r < 0 or c< 0 or r>= rows or c>=cols or board[r][c] != "O":
-                return 
-            
+            if (r not in range(rows) or c not in range(cols) or board[r][c] != "O"):
+                return
             board[r][c] = "#"
             for dr,dc in directions:
                 dfs(r+dr,c+dc)
-            
+        
         for r in range(rows):
             dfs(r,0)
             dfs(r,cols-1)
@@ -27,5 +26,5 @@ class Solution:
             for c in range(cols):
                 if board[r][c] == "O":
                     board[r][c] = "X"
-                elif board[r][c] == "#":
+                if board[r][c] == "#":
                     board[r][c] = "O"
