66e792ded2ce07c721e882cf310554369f4d4e6c
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-13 04:27:02 -0500

    File updated at 11/13/2025, 4:27:02 AM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,8 +1,9 @@
+# Last updated: 11/13/2025, 4:27:02 AM
 class Solution:
     def climbStairs(self, n: int) -> int:
         one,two = 1,1
         for i in range(n-1):
             temp = one
-            one = one + two
+            one = one+two
             two = temp
         return one
