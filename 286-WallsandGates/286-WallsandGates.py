34a04ac3193e433669a7e35a77b6d53b9431a904
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-12 20:23:47 -0500

    File updated at 11/12/2025, 8:23:47 PM

--- 286-WallsandGates/286-WallsandGates.py
+++ 286-WallsandGates/286-WallsandGates.py
@@ -1,4 +1,4 @@
-# Last updated: 11/12/2025, 8:23:27 PM
+# Last updated: 11/12/2025, 8:23:47 PM
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
@@ -14,11 +14,10 @@ class Solution:
                     q.append((r,c))
         
         while q:
-            for i in range(len(q)):
-                r,c = q.popleft()
-                for dr,dc in [1,0],[-1,0],[0,1],[0,-1]:
-                    nr,nc = r+dr,c+dc
-                    if (nr in range(rows) and nc in range(cols) and rooms[nr][nc] == inf):
-                        rooms[nr][nc] = rooms[r][c] +1
-                        q.append((nr,nc))
+            r,c = q.popleft()
+            for dr,dc in [1,0],[-1,0],[0,1],[0,-1]:
+                nr,nc = r+dr,c+dc
+                if (nr in range(rows) and nc in range(cols) and rooms[nr][nc] == inf):
+                    rooms[nr][nc] = rooms[r][c] +1
+                    q.append((nr,nc))
 
