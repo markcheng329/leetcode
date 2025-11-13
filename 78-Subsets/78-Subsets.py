f0821a0ebe6e329b0651da49aae9527c9f25207e
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-13 05:27:01 -0500

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.py
@@ -0,0 +1,18 @@
+# Last updated: 11/13/2025, 5:27:01 AM
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i == len(nums):
+                res.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            dfs(i+1)
+            subset.pop()
+            dfs(i+1)
+
+        dfs(0)
+        return res
