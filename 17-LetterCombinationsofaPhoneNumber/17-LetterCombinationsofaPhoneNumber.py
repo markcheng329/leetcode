084647c0ffbf9ca12d391f2a47d7b03ae926eac1
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-14 17:10:55 -0500

    Create 17-LetterCombinationsofaPhoneNumber

---
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
@@ -0,0 +1,29 @@
+# Last updated: 11/14/2025, 5:10:55 PM
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        path = []
+
+        phone = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def dfs(i):
+            if i == len(digits):
+                res.append("".join(path))
+                return
+            
+            for ch in phone[digits[i]]:
+                path.append(ch)
+                dfs(i+1)
+                path.pop()
+        
+        dfs(0)
+        return res
