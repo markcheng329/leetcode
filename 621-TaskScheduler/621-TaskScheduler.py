51e8aa13954ed6563dcc168fc72a6b63e1aa3d52
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-14 20:52:28 -0500

    File updated at 11/14/2025, 8:52:28 PM

--- 621-TaskScheduler/621-TaskScheduler.py
+++ 621-TaskScheduler/621-TaskScheduler.py
@@ -1,14 +1,17 @@
-# Last updated: 11/14/2025, 8:45:29 PM
+# Last updated: 11/14/2025, 8:52:28 PM
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count = [0] * 26
-        for task in tasks:
-            count[ord(task) - ord('A')] += 1
-
+        for i in range(len(tasks)):
+            count[ord(tasks[i])-ord("A")] +=1
+        
         maxf = max(count)
-        maxCount = 0
+        maxc = 0
         for i in count:
-            maxCount += 1 if i == maxf else 0
+            maxc +=1 if i == maxf else 0
+        
+        res = (maxf-1)*(n+1) + maxc
+        return max(len(tasks),res)
+        
 
-        time = (maxf - 1) * (n + 1) + maxCount
-        return max(len(tasks), time)
+
