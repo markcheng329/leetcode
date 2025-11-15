986948c3c7c74564f317749aa0f4cc0243181d05
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-14 22:03:23 -0500

    File updated at 11/14/2025, 10:03:23 PM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,19 +1,21 @@
-# Last updated: 11/13/2025, 6:23:24 AM
+# Last updated: 11/14/2025, 10:03:23 PM
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        subset = []
+        path = []
 
         def backtracking(start,remain):
             if remain == 0:
-                res.append(subset.copy())
+                res.append(path.copy())
+            
+            if remain < 0:
+                return
             
             for i in range(start,len(candidates)):
-                if remain < 0:
-                    return
-                subset.append(candidates[i])
+
+                path.append(candidates[i])
                 backtracking(i,remain-candidates[i])
-                subset.pop()
+                path.pop()
         
         backtracking(0,target)
         return res
