d8e4f4e90dcfca4f112d541e324d3ffb879c0ced
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-14 22:40:03 -0500

    File updated at 11/14/2025, 10:40:03 PM

--- 48-RotateImage/48-RotateImage.py
+++ 48-RotateImage/48-RotateImage.py
@@ -1,4 +1,4 @@
-# Last updated: 11/14/2025, 10:36:39 PM
+# Last updated: 11/14/2025, 10:40:03 PM
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
@@ -7,11 +7,10 @@ class Solution:
         l, r = 0,len(matrix) -1
         while l < r:         
             for i in range(r-l):
-                # top,bottom = l,r
                 topleft = matrix[l][l+i]
                 matrix[l][l+i] = matrix[r-i][l]
                 matrix[r-i][l] = matrix[r][r-i]
                 matrix[r][r-i] = matrix[l+i][r]
-                matrix[l+i][r]= topleft
+                matrix[l+i][r] = topleft
             l+=1
             r-=1
