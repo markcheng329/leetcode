0dbb3c8cf4714fc53d52e40a969122d64ef409a9
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-18 23:37:39 -0500

    File updated at 11/18/2025, 11:37:39 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,23 +1,22 @@
+# Last updated: 11/18/2025, 11:37:39 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         res = []
-
+        
         def dfs(node,depth):
             if not node:
                 return None
             
             if len(res) == depth:
                 res.append([])
-            
             res[depth].append(node.val)
+
             dfs(node.left,depth+1)
             dfs(node.right,depth+1)
         
