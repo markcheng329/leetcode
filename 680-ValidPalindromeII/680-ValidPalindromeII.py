ed2ac634f494b6d78900ec27675e96d2d589d639
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-29 01:17:32 -0500

    File updated at 11/29/2025, 1:17:32 AM

--- 680-ValidPalindromeII/680-ValidPalindromeII.py
+++ 680-ValidPalindromeII/680-ValidPalindromeII.py
@@ -1,24 +1,25 @@
-# Last updated: 11/29/2025, 1:06:54 AM
+# Last updated: 11/29/2025, 1:17:32 AM
 1class Solution:
 2    def validPalindrome(self, s: str) -> bool:
-3        def is_pal(l: int, r: int) -> bool:
-4            # 判断 s[l:r+1] 是否是回文
-5            while l < r:
-6                if s[l] != s[r]:
-7                    return False
-8                l += 1
-9                r -= 1
-10            return True
-11
-12        l, r = 0, len(s) - 1
-13
-14        while l < r:
-15            if s[l] == s[r]:
-16                l += 1
-17                r -= 1
-18            else:
-19                # 删除一次：要么删左边，要么删右边
-20                return is_pal(l + 1, r) or is_pal(l, r - 1)
-21
-22        return True
-23
+3        l , r = 0,len(s)-1
+4        while l < r:
+5            if s[l] == s[r]:
+6                l +=1
+7                r-=1
+8            else:
+9                if self.ispal(s,l+1,r) or self.ispal(s,l,r-1):
+10                    return True
+11                else:
+12                    return False
+13        return True
+14    
+15
+16
+17    def ispal(self,s,l,r):
+18        while l < r:
+19            if s[l] != s[r]:
+20                return False
+21            else:
+22                l +=1
+23                r -=1
+24        return True
