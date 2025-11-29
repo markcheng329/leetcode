e53df01a1022ac1ae8a7802bac398154b1185f89
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-29 01:23:09 -0500

    File updated at 11/29/2025, 1:23:08 AM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,15 +1,18 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        i = j = 0
-
-        res = []
-
-        while i < len(word1) or j<len(word2):
-            if i < len(word1):
-                res.append(word1[i])
-                i+=1
-            if j < len(word2):
-                res.append(word2[j])
-                j+=1
-        
-        return "".join(res)
+# Last updated: 11/29/2025, 1:23:08 AM
+1class Solution:
+2    def mergeAlternately(self, word1: str, word2: str) -> str:
+3        i , j = 0,0
+4        res = []
+5
+6        while i < len(word1) or i < len(word2):
+7            if i < len(word1):
+8                res.append(word1[i])
+9            if j < len(word2):
+10                res.append(word2[j])
+11            
+12            i +=1
+13            j +=1
+14        return "".join(res)
+15
+16
+17
