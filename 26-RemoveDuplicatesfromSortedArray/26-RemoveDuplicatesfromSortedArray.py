28e1458e05c24cca41e4bb37360109a45efdde10
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-29 01:50:44 -0500

    File updated at 11/29/2025, 1:50:44 AM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,10 +1,13 @@
-# Last updated: 11/29/2025, 1:50:14 AM
+# Last updated: 11/29/2025, 1:50:44 AM
 1class Solution:
 2    def removeDuplicates(self, nums: List[int]) -> int:
-3        slow = 1
-4
-5        for fast in range(1,len(nums)):
-6            if nums[fast] != nums[slow-1]:
-7                nums[slow] = nums[fast]
-8                slow +=1
-9        return slow
+3        if not nums:
+4            return 0
+5            
+6        slow = 1
+7
+8        for fast in range(1,len(nums)):
+9            if nums[fast] != nums[slow-1]:
+10                nums[slow] = nums[fast]
+11                slow +=1
+12        return slow
