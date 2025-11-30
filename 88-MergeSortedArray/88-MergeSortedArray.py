d1c6be5174d805ff01f2794bbe4f4d43676d1652
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-30 02:03:47 -0500

    File updated at 11/30/2025, 2:03:47 AM

--- 88-MergeSortedArray/88-MergeSortedArray.py
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -1,21 +1,24 @@
-# Last updated: 9/23/2025, 2:13:19 AM
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        last = m+n-1
-        while m > 0 and n> 0:
-            if nums1[m-1] > nums2[n-1]:
-                nums1[last] = nums1[m-1]
-                m-=1
-                last -=1
-            else:
-                nums1[last] = nums2[n-1]
-                n -=1
-                last -=1
-        
-        while n > 0:
-            nums1[last] = nums2[n-1]
-            n-=1
-            last -=1
+# Last updated: 11/30/2025, 2:03:47 AM
+1class Solution:
+2    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+3        """
+4        Do not return anything, modify nums1 in-place instead.
+5        """
+6        last = m + n -1
+7
+8        while m > 0 and n > 0:
+9            if nums1[m-1] > nums2[n-1]:
+10                nums1[last] = nums1[m-1]
+11                m-=1
+12            else:
+13                nums1[last] = nums2[n-1]
+14                n-=1
+15
+16            last -=1
+17        
+18        while n > 0:
+19            nums1[last] = nums2[n-1]
+20            last -=1
+21            n-=1
+22        
+23        return nums1
