a2ff68d5c110b96e524dfc4d62b5692acabe6716
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-30 04:53:35 -0500

    File updated at 11/30/2025, 4:53:35 AM

--- 189-RotateArray/189-RotateArray.py
+++ 189-RotateArray/189-RotateArray.py
@@ -1,4 +1,4 @@
-# Last updated: 11/30/2025, 4:52:01 AM
+# Last updated: 11/30/2025, 4:53:35 AM
 1class Solution:
 2    def rotate(self, nums: List[int], k: int) -> None:
 3        """
@@ -6,16 +6,17 @@
 5        """
 6        if len(nums) == 1:
 7            return nums
-8        k = k % len(nums)
-9
-10        self.reverse(nums,0,len(nums)-1)
-11        self.reverse(nums,0,k-1)
-12        self.reverse(nums,k,len(nums)-1)
-13
-14    def reverse(self,s,l,r):
-15        while l < r:
-16            s[l],s[r] = s[r],s[l]
-17            l+=1 
-18            r-=1
-19        
-20
+8            
+9        k = k % len(nums)
+10
+11        self.reverse(nums,0,len(nums)-1)
+12        self.reverse(nums,0,k-1)
+13        self.reverse(nums,k,len(nums)-1)
+14
+15    def reverse(self,s,l,r):
+16        while l < r:
+17            s[l],s[r] = s[r],s[l]
+18            l+=1 
+19            r-=1
+20        
+21
