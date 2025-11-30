ba217a6d5f1c2faaa9077d0c795ce097a069ae22
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-30 05:10:31 -0500

    File updated at 11/30/2025, 5:10:31 AM

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,19 +1,18 @@
-# Last updated: 11/18/2025, 10:42:14 PM
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        l, r = 0,len(height)-1
-        res = 0
-        lmax = height[l]
-        rmax = height[r]
-
-        while l < r:
-            if lmax < rmax:
-                l +=1
-                lmax = max(lmax,height[l])
-                res += lmax-height[l]
-            else:
-                r -=1
-                rmax = max(rmax,height[r])
-                res += rmax - height[r]
-        return res
-            
+# Last updated: 11/30/2025, 5:10:31 AM
+1class Solution:
+2    def trap(self, height: List[int]) -> int:
+3        l, r = 0,len(height)-1
+4        lmax,rmax = height[l],height[r]
+5        res = 0
+6
+7        while l < r :
+8            if height[l] < height[r]:
+9                l +=1
+10                lmax = max(lmax,height[l])
+11                res += lmax - height[l]
+12            else:
+13                r -=1
+14                rmax = max(rmax,height[r])
+15                res += rmax-height[r]
+16        return res
+17
