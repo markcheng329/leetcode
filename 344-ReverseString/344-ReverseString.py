867ed22da343eb51e65887754f98a2878287feaf
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-30 01:43:01 -0500

    File updated at 11/30/2025, 1:43:01 AM

--- 344-ReverseString/344-ReverseString.py
+++ 344-ReverseString/344-ReverseString.py
@@ -1,14 +1,13 @@
-# Last updated: 11/28/2025, 10:43:15 PM
+# Last updated: 11/30/2025, 1:43:01 AM
 1class Solution:
 2    def reverseString(self, s: List[str]) -> None:
 3        """
 4        Do not return anything, modify s in-place instead.
 5        """
-6        l,r = 0,len(s)-1
-7        
+6        l, r = 0, len(s)-1
+7
 8        while l < r:
 9            s[l],s[r] = s[r],s[l]
 10            l +=1
-11            r -=1
+11            r-=1
 12        
-13        return s
