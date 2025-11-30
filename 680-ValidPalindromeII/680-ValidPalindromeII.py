fde3781ad75d0a5ef0c1f3738da058f72448561e
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-11-30 01:47:46 -0500

    File updated at 11/30/2025, 1:47:46 AM

--- 680-ValidPalindromeII/680-ValidPalindromeII.py
+++ 680-ValidPalindromeII/680-ValidPalindromeII.py
@@ -1,25 +1,24 @@
-# Last updated: 11/29/2025, 1:17:32 AM
+# Last updated: 11/30/2025, 1:47:46 AM
 1class Solution:
 2    def validPalindrome(self, s: str) -> bool:
-3        l , r = 0,len(s)-1
-4        while l < r:
-5            if s[l] == s[r]:
-6                l +=1
-7                r-=1
-8            else:
-9                if self.ispal(s,l+1,r) or self.ispal(s,l,r-1):
-10                    return True
-11                else:
-12                    return False
-13        return True
-14    
-15
-16
-17    def ispal(self,s,l,r):
-18        while l < r:
-19            if s[l] != s[r]:
-20                return False
-21            else:
-22                l +=1
-23                r -=1
-24        return True
+3        l,r = 0,len(s)-1
+4
+5        while l < r:
+6            if s[l] != s[r]:
+7                if self.ispal(s,l+1,r) or self.ispal(s,l,r-1):
+8                    return True
+9                else:
+10                    return False            
+11            else:
+12                l +=1
+13                r -=1
+14        return True
+15    
+16    def ispal(self,s,l,r):
+17        while l < r:
+18            if s[l] != s[r]:
+19                return False
+20            else:
+21                l +=1
+22                r-=1
+23        return True
