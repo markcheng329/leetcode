9298ce442971120d6c69ee4396dc21bab9672aa1
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-02 02:44:07 -0500

    File updated at 12/2/2025, 2:44:07 AM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,26 +1,25 @@
-# Last updated: 12/2/2025, 2:34:05 AM
+# Last updated: 12/2/2025, 2:44:07 AM
 1class Solution:
 2    def checkInclusion(self, s1: str, s2: str) -> bool:
-3
-4        if len(s1) > len(s2):
-5            return False
-6        
-7        have = [0] * 26
-8        need = [0] * 26
-9
-10        for i in range(len(s1)):
-11            need[ord(s1[i])-ord("a")] +=1
-12        
-13        for i in range(len(s1)):
-14            have[ord(s2[i]) - ord ("a")] +=1
-15
-16        if need == have:
-17            return True
-18        
-19        for i in range(len(s1),len(s2)):
-20            have[ord(s2[i]) - ord ("a")] +=1
-21            have[ord(s2[i-len(s1)]) - ord ("a")] -=1
-22
-23            if have == need:
-24                return True
-25        return False
+3        if len(s1) > len(s2):
+4            return False
+5        
+6        have = [0] * 26
+7        need = [0] * 26
+8
+9        for i in range(len(s1)):
+10            need[ord(s1[i])-ord("a")] +=1
+11        
+12        for i in range(len(s1)):
+13            have[ord(s2[i])-ord("a")] +=1
+14        
+15        if need == have:
+16            return True
+17        
+18        for i in range(len(s1),len(s2)):
+19            have[ord(s2[i])-ord("a")] +=1
+20            have[ord(s2[i-len(s1)])-ord("a")] -=1
+21            
+22            if have == need:
+23                return True
+24        return False
