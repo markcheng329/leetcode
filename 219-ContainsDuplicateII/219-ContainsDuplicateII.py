2972ea21b9f784946ac62285f44cbfa645918523
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-03 20:54:39 -0500

    File updated at 12/3/2025, 8:54:39 PM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.py
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.py
@@ -1,14 +1,18 @@
-class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        window = set()
-        l=0
-
-        for i in range(len(nums)):
-            if i-l > k:
-                window.remove(nums[l])
-                l+=1
-            if nums[i] in window:
-                return True
-            window.add(nums[i])
-        return False
-        
+# Last updated: 12/3/2025, 8:54:39 PM
+1class Solution:
+2    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+3        
+4        seen = set()
+5        l = 0
+6        
+7        for i in range(len(nums)):
+8            if i - l > k:
+9                seen.remove(nums[l])
+10                l +=1
+11            
+12            if nums[i] in seen:
+13                return True
+14            
+15            seen.add(nums[i])
+16        return False
+17
