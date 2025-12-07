7fd84a40f470b43a64af90df9ed671eff6a3c375
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-07 00:00:47 -0500

    File updated at 12/7/2025, 12:00:46 AM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,16 +1,17 @@
-# Last updated: 11/25/2025, 1:30:37 AM
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        res = 0
-        count = 0
-
-        for i in range(len(nums)):
-            if count == 0:
-                res = nums[i]
-            
-            if nums[i] == res:
-                count +=1
-            else:
-                count -=1
-        return res
-                
+# Last updated: 12/7/2025, 12:00:46 AM
+1class Solution:
+2    def majorityElement(self, nums: List[int]) -> int:
+3        res = 0
+4        count = 0
+5
+6        for i in range(len(nums)):
+7            if count == 0:
+8                res = nums[i]
+9            
+10            if nums[i] == res:
+11                count +=1
+12            else:
+13                count -=1
+14
+15        return res
+16        
