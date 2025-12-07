12ee6405e2bd074ec8a1fb20b5918e7aa093831f
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-06 22:32:12 -0500

    File updated at 12/6/2025, 10:32:12 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,11 +1,12 @@
-# Last updated: 11/25/2025, 1:17:29 AM
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        first = strs[0]
-
-        for i in range(len(first)):
-            ch = first[i]
-            for s in strs:
-                if i == len(s) or ch != s[i]:
-                    return first[:i]
-        return first
+# Last updated: 12/6/2025, 10:32:12 PM
+1class Solution:
+2    def longestCommonPrefix(self, strs: List[str]) -> str:
+3        res = ""
+4
+5        for i in range(len(strs[0])):
+6            for s in strs:
+7                if i == len(s) or s[i] != strs[0][i]:
+8                    return res
+9            res += strs[0][i]
+10        return res
+11
