de521b3a499959fbf1ab405f2e10e3b1e0ab4599
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-06 22:14:43 -0500

    File updated at 12/6/2025, 10:14:43 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,10 +1,9 @@
-# Last updated: 11/24/2025, 10:16:58 PM
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        res = [0]* n * 2
-
-        for i in range(n):
-            res[i] = nums[i]
-            res[i+n] = nums[i]
-        return res
+# Last updated: 12/6/2025, 10:14:43 PM
+1class Solution:
+2    def getConcatenation(self, nums: List[int]) -> List[int]:
+3        res = [0] * len(nums) * 2
+4
+5        for i in range(len(nums)):
+6            res[i] = nums[i]
+7            res [i+len(nums)] = nums[i]
+8        return res
