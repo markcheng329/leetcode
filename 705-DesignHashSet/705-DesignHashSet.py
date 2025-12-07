b09f72168c0ebd05b3538169efda9ebfb2035808
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-07 00:10:00 -0500

    File updated at 12/7/2025, 12:10:00 AM

--- 705-DesignHashSet/705-DesignHashSet.py
+++ 705-DesignHashSet/705-DesignHashSet.py
@@ -1,25 +1,27 @@
-# Last updated: 12/7/2025, 12:08:54 AM
+# Last updated: 12/7/2025, 12:10:00 AM
 1class MyHashSet:
 2
 3    def __init__(self):
-4        self.data = [False] * (10**6 +1)
+4        self.data = []
 5        
 6
 7    def add(self, key: int) -> None:
-8        self.data[key] = True
-9        
-10
-11    def remove(self, key: int) -> None:
-12        self.data[key] = False
-13        
-14
-15    def contains(self, key: int) -> bool:
-16        return self.data[key]
-17        
-18
-19
-20# Your MyHashSet object will be instantiated and called as such:
-21# obj = MyHashSet()
-22# obj.add(key)
-23# obj.remove(key)
-24# param_3 = obj.contains(key)
+8        if key not in self.data:
+9            self.data.append(key)
+10        
+11
+12    def remove(self, key: int) -> None:
+13        if key in self.data:
+14            self.data.remove(key)
+15        
+16
+17    def contains(self, key: int) -> bool:
+18        return key in self.data
+19        
+20
+21
+22# Your MyHashSet object will be instantiated and called as such:
+23# obj = MyHashSet()
+24# obj.add(key)
+25# obj.remove(key)
+26# param_3 = obj.contains(key)
