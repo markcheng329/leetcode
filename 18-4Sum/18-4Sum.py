c86ac81fd24440d853a3bc276d8876e74f03ebfb
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-09 00:54:41 -0500

    File updated at 12/9/2025, 12:54:40 AM

--- 18-4Sum/18-4Sum.py
+++ 18-4Sum/18-4Sum.py
@@ -1,30 +1,30 @@
-# Last updated: 12/1/2025, 11:37:19 PM
+# Last updated: 12/9/2025, 12:54:40 AM
 1class Solution:
 2    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
 3        nums.sort()
 4        res = []
-5        
+5
 6        for i in range(len(nums)):
 7            if i > 0 and nums[i] == nums[i-1]:
 8                continue
 9            for j in range(i+1,len(nums)):
 10                if j > i+1 and nums[j] == nums[j-1]:
 11                    continue
-12                
-13                l , r = j+1,len(nums)-1
-14                while l < r:
-15                    total = nums[i] + nums[j] + nums[l] + nums[r]
-16                    if total > target:
-17                        r-=1
-18                    elif total < target:
-19                        l +=1
-20                    else:
-21                        res.append([nums[i],nums[j],nums[l],nums[r]])
-22                        l +=1
-23                        r -=1
-24                        while l < r and nums[l] == nums[l-1]:
-25                            l+=1
-26                        while l < r and nums[r] == nums[r+1]:
-27                            r -=1
-28        return res
-29
+12                    
+13                l, r = j+1,len(nums)-1
+14
+15                while l < r:
+16                    total = nums[i] + nums[j] + nums[l] + nums[r]
+17                    if total > target:
+18                        r -=1
+19                    elif total < target:
+20                        l +=1
+21                    else:
+22                        res.append([nums[i],nums[j],nums[l],nums[r]])
+23                        l +=1
+24                        r-=1
+25                        while l < r and nums[l] == nums[l-1]:
+26                            l +=1
+27                        while l < r and nums[r] == nums[r+1]:
+28                            r -=1
+29        return res
