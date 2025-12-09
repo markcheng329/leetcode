c6922293ca59d363d3af22367dbf7a58bc0c39d8
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-09 01:18:20 -0500

    File updated at 12/9/2025, 1:18:20 AM

--- 881-BoatstoSavePeople/881-BoatstoSavePeople.py
+++ 881-BoatstoSavePeople/881-BoatstoSavePeople.py
@@ -1,16 +1,17 @@
-# Last updated: 12/2/2025, 12:26:59 AM
+# Last updated: 12/9/2025, 1:18:20 AM
 1class Solution:
 2    def numRescueBoats(self, people: List[int], limit: int) -> int:
-3        people.sort()
-4        l, r = 0,len(people)-1
-5        res = 0
+3        res = 0
+4        people.sort()
+5        l, r = 0,len(people)-1
 6
-7
-8        while l <= r:
-9            if people[l] + people[r] <= limit:
-10                l +=1
-11                r-=1
+7        while l <=r:
+8            if people[l] + people[r] <= limit:
+9                l+=1
+10                r-=1
+11                res +=1
 12            else:
 13                r-=1
-14            res +=1
+14                res +=1
 15        return res
+16
