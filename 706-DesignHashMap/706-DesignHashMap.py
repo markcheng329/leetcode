1850153069a98366e0b880e2ccba3cca0a4cd82b
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-10 23:23:32 -0500

    File updated at 12/10/2025, 11:23:32 PM

--- 706-DesignHashMap/706-DesignHashMap.py
+++ 706-DesignHashMap/706-DesignHashMap.py
@@ -1,24 +1,25 @@
-# Last updated: 12/7/2025, 12:12:58 AM
+# Last updated: 12/10/2025, 11:23:32 PM
 1class MyHashMap:
 2
 3    def __init__(self):
-4        self.map = [-1] * ( 10**6 +1)
+4        self.map = [-1] * (10**6+1)
 5        
 6
 7    def put(self, key: int, value: int) -> None:
 8        self.map[key] = value
 9        
-10    def get(self, key: int) -> int:
-11        return self.map[key]
-12        
-13
-14    def remove(self, key: int) -> None:
-15        self.map[key] = -1
-16        
-17
+10
+11    def get(self, key: int) -> int:
+12        return self.map[key]
+13        
+14
+15    def remove(self, key: int) -> None:
+16        self.map[key] = -1
+17        
 18
-19# Your MyHashMap object will be instantiated and called as such:
-20# obj = MyHashMap()
-21# obj.put(key,value)
-22# param_2 = obj.get(key)
-23# obj.remove(key)
+19
+20# Your MyHashMap object will be instantiated and called as such:
+21# obj = MyHashMap()
+22# obj.put(key,value)
+23# param_2 = obj.get(key)
+24# obj.remove(key)
