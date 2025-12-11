dc8a158f8660f2ec2d63f36870baae8638ff7161
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-11 01:58:46 -0500

    File updated at 12/11/2025, 1:58:46 AM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.py
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.py
@@ -1,10 +1,9 @@
-# Last updated: 9/30/2025, 3:01:13 AM
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-
-        for i in range(1,len(prices)):
-            if prices[i] > prices[i-1]:
-                profit += prices[i] - prices[i-1]
-        
-        return profit
+# Last updated: 12/11/2025, 1:58:46 AM
+1class Solution:
+2    def maxProfit(self, prices: List[int]) -> int:
+3        total = 0
+4        
+5        for i in range(1,len(prices)):
+6            if prices[i] > prices[i-1]:
+7                total += prices[i] - prices[i-1]
+8        return total
