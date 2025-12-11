e28da4f5bc7633e4df8997e2524e0cd5bb983359
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-10 23:53:18 -0500

    File updated at 12/10/2025, 11:53:18 PM

--- 912-SortanArray/912-SortanArray.py
+++ 912-SortanArray/912-SortanArray.py
@@ -1,22 +1,22 @@
-# Last updated: 12/7/2025, 1:08:45 AM
+# Last updated: 12/10/2025, 11:53:18 PM
 1class Solution:
 2    def sortArray(self, nums: List[int]) -> List[int]:
-3        if len(nums) <= 1:
+3        if len (nums) <= 1:
 4            return nums
 5        
-6        mid = (len(nums))//2
+6        mid = (len(nums)) //2
 7
 8        left = self.sortArray(nums[:mid])
 9        right = self.sortArray(nums[mid:])
-10
+10    
 11        return self.merge(left,right)
-12    
+12
 13    def merge(self,left,right):
-14        res = []
-15        i = j = 0
+14        i = j = 0
+15        res = []
 16
 17        while i < len(left) and j < len(right):
-18            if left[i] <=right[j]:
+18            if left[i] < right[j]:
 19                res.append(left[i])
 20                i +=1
 21            else:
@@ -24,11 +24,13 @@
 23                j +=1
 24        
 25        while i < len(left):
-26                res.append(left[i])
-27                i +=1
-28            
+26            res.append(left[i])
+27            i +=1
+28
 29        while j < len(right):
-30                res.append(right[j])
-31                j +=1
-32        return res
-33
+30            res.append(right[j])
+31            j +=1
+32
+33        return res
+34
+35
