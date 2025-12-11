a7b5b899a8196784876380d22208b93df6944448
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-10 21:05:17 -0500

    File updated at 12/10/2025, 9:05:16 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,11 +1,12 @@
-# Last updated: 12/6/2025, 10:28:08 PM
+# Last updated: 12/10/2025, 9:05:16 PM
 1class Solution:
 2    def twoSum(self, nums: List[int], target: int) -> List[int]:
 3        seen = {}
 4
 5        for i in range(len(nums)):
-6            diff = target-nums[i]
+6            diff = target - nums[i]
 7            if diff in seen:
 8                return[seen[diff],i]
-9            seen[nums[i]] = i
-10        
+9            else:
+10                seen[nums[i]] = i
+11        
