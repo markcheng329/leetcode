25204b136a85e8178cc34d73d9fad8f1ad712baa
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-10 21:24:05 -0500

    File updated at 12/10/2025, 9:24:05 PM

--- 27-RemoveElement/27-RemoveElement.py
+++ 27-RemoveElement/27-RemoveElement.py
@@ -1,10 +1,10 @@
-# Last updated: 12/6/2025, 11:53:18 PM
+# Last updated: 12/10/2025, 9:24:05 PM
 1class Solution:
 2    def removeElement(self, nums: List[int], val: int) -> int:
 3        slow = 0
 4
 5        for i in range(len(nums)):
 6            if nums[i] != val:
-7                nums[slow] = nums[i]
+7                nums[slow]= nums[i]
 8                slow +=1
 9        return slow
