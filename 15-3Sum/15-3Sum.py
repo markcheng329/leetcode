b13ace558f92725ec924c7bf5a906eb7c869a3fb
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-18 00:17:12 -0500

    File updated at 12/18/2025, 12:17:12 AM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,28 +1,28 @@
-# Last updated: 11/30/2025, 4:39:03 AM
+# Last updated: 12/18/2025, 12:17:12 AM
 1class Solution:
 2    def threeSum(self, nums: List[int]) -> List[List[int]]:
 3        nums.sort()
 4        res = []
-5        for i in range(len(nums)):
-6            if nums[i] > 0:
-7                break
-8            if i > 0 and nums[i] == nums[i-1]:
-9                continue
-10            
-11            l, r = i+1,len(nums)-1
-12
-13            while l < r:
-14                total = nums[i] + nums[l] + nums[r]
-15                if total > 0:
-16                    r -=1
-17                elif total < 0:
-18                    l +=1
-19                else:
-20                    res.append([nums[i],nums[l],nums[r]])
-21                    l +=1
-22                    r-=1
-23                    while l < r and nums[l] == nums[l-1]:
-24                        l +=1
-25                    while l < r and nums[r] == nums[r+1]:
-26                        r -=1
-27        return res
+5
+6        for i in range(len(nums)):
+7            if nums[i] > 0 :
+8                break          
+9            if i > 0 and nums[i] == nums[i-1]:
+10                continue
+11            
+12            l,r = i+1,len(nums)-1
+13            
+14            while l < r:
+15                total = nums[i] + nums[l] + nums[r]
+16                if total > 0:
+17                    r-=1
+18                elif total < 0:
+19                    l +=1
+20                else:
+21                    res.append([nums[i],nums[l],nums[r]])
+22                    l +=1
+23                    r-=1
+24                    while l < r and nums[l] == nums[l-1]:
+25                        l +=1
+26        return res
+27            
