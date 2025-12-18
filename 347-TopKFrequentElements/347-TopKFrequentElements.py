ade80d64caee9574d08dc81bb5382ed90a6124a0
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-17 22:20:31 -0500

    File updated at 12/17/2025, 10:20:31 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,4 +1,4 @@
-# Last updated: 12/17/2025, 10:18:40 PM
+# Last updated: 12/17/2025, 10:20:31 PM
 1class Solution:
 2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 3        freq = [[]for i in range(len(nums)+1)]
@@ -6,7 +6,7 @@
 5        count = Counter(nums)
 6        res = []
 7
-8        for num,c in count.items():
+8        for num, c in count.items():
 9            freq[c].append(num)
 10        
 11        for i in range(len(freq)-1,-1,-1):
