394aa331b799f26f798efbd7c413aa0ef45596c2
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-18 00:53:13 -0500

    File updated at 12/18/2025, 12:53:13 AM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,11 +1,11 @@
-# Last updated: 12/3/2025, 9:06:37 PM
+# Last updated: 12/18/2025, 12:53:13 AM
 1class Solution:
 2    def checkInclusion(self, s1: str, s2: str) -> bool:
 3        if len(s1) > len(s2):
 4            return False
 5        
-6        have = [0] * 26
-7        need = [0] * 26
+6        need = [0] * 26
+7        have = [0] * 26
 8
 9        for i in range(len(s1)):
 10            need[ord(s1[i])-ord("a")] +=1
@@ -18,9 +18,8 @@
 17        
 18        for i in range(len(s1),len(s2)):
 19            have[ord(s2[i])-ord("a")] +=1
-20            have[ord(s2[i-len(s1)])-ord("a")] -=1
-21
-22            if have == need:
-23                return True
-24        return False
-25
+20            have[ord(s2[i-len(s1)]) - ord("a")] -=1
+21            if need == have:
+22                return True
+23        return False
+24        
