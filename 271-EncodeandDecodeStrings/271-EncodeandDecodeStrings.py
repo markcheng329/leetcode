b0bb09c01ab098ff3fde4f57a8705c2d2013b5e2
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-27 00:20:55 -0500

    File updated at 12/27/2025, 12:20:55 AM

--- 271-EncodeandDecodeStrings/271-EncodeandDecodeStrings.py
+++ 271-EncodeandDecodeStrings/271-EncodeandDecodeStrings.py
@@ -1,36 +1,33 @@
-# Last updated: 12/17/2025, 10:24:59 PM
+# Last updated: 12/27/2025, 12:20:55 AM
 1class Codec:
 2    def encode(self, strs: List[str]) -> str:
 3        """Encodes a list of strings to a single string.
 4        """
-5        # abced = 5#abced
-6        res = ""
-7        for s in strs:
-8            res += str(len(s)) + "#" + s
-9        return res
-10        
-11
-12    def decode(self, s: str) -> List[str]:
-13        """Decodes a single string to a list of strings.
-14        """
-15        #5#abced
+5        res= []
+6        for s in strs:
+7            res += str(len(s)) + "#" + s
+8        return "".join(res)
+9        
+10
+11    def decode(self, s: str) -> List[str]:
+12        """Decodes a single string to a list of strings.
+13        """
+14        #  "5#abcde3#abc"
+15        res = []
 16        i = 0
-17        res = []
-18        while i < len(s):
-19            j = i
-20            while s[j] != "#":
-21                j +=1
-22            length = int(s[i:j])
-23            i = j+1
-24            j = i + length
-25            res.append(s[i:j])
-26            i = j
-27        return res
+17        while i < len(s):
+18            j = i
+19            while s[j] != "#":
+20                j +=1
+21            length = int(s[i:j])
+22            i = j+1
+23            j = i + length
+24            res.append(s[i:j])
+25            i = j
+26        return res
+27        
 28
 29
-30        
-31
-32
-33# Your Codec object will be instantiated and called as such:
-34# codec = Codec()
-35# codec.decode(codec.encode(strs))
+30# Your Codec object will be instantiated and called as such:
+31# codec = Codec()
+32# codec.decode(codec.encode(strs))
