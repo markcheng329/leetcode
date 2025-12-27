56204ea6b3e0a734e5befbcd3a8a201355de697c
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-27 00:22:20 -0500

    File updated at 12/27/2025, 12:22:20 AM

--- 271-EncodeandDecodeStrings/271-EncodeandDecodeStrings.py
+++ 271-EncodeandDecodeStrings/271-EncodeandDecodeStrings.py
@@ -1,4 +1,4 @@
-# Last updated: 12/27/2025, 12:21:53 AM
+# Last updated: 12/27/2025, 12:22:20 AM
 1class Codec:
 2    def encode(self, strs: List[str]) -> str:
 3        """Encodes a list of strings to a single string.
