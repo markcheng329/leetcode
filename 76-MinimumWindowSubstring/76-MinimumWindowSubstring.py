5d3b2b5b620f4d705db397cb2b10777daeb7709f
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-27 02:29:56 -0500

    File updated at 12/27/2025, 2:29:56 AM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,28 +1,27 @@
-# Last updated: 12/18/2025, 1:10:57 AM
+# Last updated: 12/27/2025, 2:29:56 AM
 1class Solution:
 2    def minWindow(self, s: str, t: str) -> str:
-3        if len(t) >len(s):
+3        if len(t) > len(s):
 4            return ""
-5        
-6        res = ""
-7        l = 0
-8        best_l = 0
-9        best_length = float('inf')
-10        missing = len(t)
-11        count = Counter(t)
-12        
-13        for i in range(len(s)):
-14            count[s[i]] -=1
-15            if count[s[i]] >= 0 :
-16                missing -=1
-17            
-18            while missing == 0:
-19                if i-l +1 < best_length:
-20                    best_length = i-l+1
-21                    best_l = l
-22                
-23                count[s[l]] +=1
-24                if count[s[l]] > 0:
-25                    missing +=1
-26                l +=1
-27        return res if best_length == float("inf") else s[best_l:best_l+best_length]
+5
+6        l = 0
+7        best_l = 0
+8        best_length = float("inf")
+9        missing = len(t)
+10        count = Counter(t)
+11
+12        for i in range(len(s)):
+13            count[s[i]] -=1
+14            if count[s[i]] >= 0:
+15                missing -=1
+16            
+17            while missing == 0:
+18                if i-l+1 < best_length:
+19                    best_length = i-l+1
+20                    best_l = l
+21                
+22                count[s[l]] +=1
+23                if count[s[l]] > 0:
+24                    missing +=1
+25                l +=1
+26        return "" if best_length == float("inf") else s[best_l:best_l+best_length]
