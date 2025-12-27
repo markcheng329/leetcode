11a349431b3bac5ff8395f5b2119756fd767b2e8
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-26 23:27:21 -0500

    File updated at 12/26/2025, 11:27:20 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,11 +1,11 @@
-# Last updated: 12/17/2025, 10:02:10 PM
+# Last updated: 12/26/2025, 11:27:20 PM
 1class Solution:
 2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-3        group = defaultdict(list)
-4        
+3        groups = defaultdict(list)
+4
 5        for s in strs:
 6            count = [0] * 26
 7            for i in s:
-8                count[ord(i)-ord("a")] +=1
-9            group[tuple(count)].append(s)
-10        return list(group.values())
+8                count[ord(i) - ord("a")] +=1
+9            groups[tuple(count)].append(s)
+10        return list(groups.values())
