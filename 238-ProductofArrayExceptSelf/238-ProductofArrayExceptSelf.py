44b0d569bd17cd5f29620f1004a638161f20bd60
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-27 00:25:10 -0500

    File updated at 12/27/2025, 12:25:10 AM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,4 +1,4 @@
-# Last updated: 12/17/2025, 10:29:49 PM
+# Last updated: 12/27/2025, 12:25:10 AM
 1class Solution:
 2    def productExceptSelf(self, nums: List[int]) -> List[int]:
 3        res = [0] * len(nums)
@@ -7,11 +7,10 @@
 6        for i in range(len(nums)):
 7            res[i] = prefix
 8            prefix = prefix * nums[i]
-9
+9        
 10        postfix = 1
 11        for i in range(len(nums)-1,-1,-1):
 12            res[i] = res[i] * postfix
 13            postfix = postfix * nums[i]
 14
 15        return res
-16
