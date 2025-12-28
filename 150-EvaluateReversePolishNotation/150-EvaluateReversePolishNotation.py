74e3a6713794cda5038db76daac75ba0158615c0
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-28 05:56:20 -0500

    File updated at 12/28/2025, 5:56:20 AM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
@@ -1,27 +1,24 @@
-# Last updated: 11/12/2025, 4:58:49 AM
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-
-        for i in tokens:
-            if i not in {"+","-","*","/"}:
-                stack.append(int(i))
-                continue
-        
-            a = stack.pop()
-            b = stack.pop()
-
-
-            if i == "+":
-                stack.append(b+a)
-            elif i == "-":
-                stack.append(b-a)
-            elif i == "*":
-                stack.append(b*a)
-            else:
-                stack.append(int(b/a))
-
-        return stack[-1]
-            
-
-        
+# Last updated: 12/28/2025, 5:56:20 AM
+1class Solution:
+2    def evalRPN(self, tokens: List[str]) -> int:
+3        stack = []
+4
+5        for i in range(len(tokens)):
+6            if tokens[i] not in ["+","-","*","/"]:
+7                stack.append(int(tokens[i]))
+8                continue
+9
+10            a = stack.pop()
+11            b = stack.pop()
+12            
+13            if tokens[i] == "+":
+14                stack.append(b+a)       
+15            elif tokens[i] == "-":
+16                stack.append(b-a)
+17            elif tokens[i] == "*":
+18                stack.append(b*a)
+19            else:
+20                stack.append(int(b/a))
+21
+22        return stack[-1]
+23
