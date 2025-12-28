7299c1dc14fd2d8bd3b53ae29c7ca6f45dea7e56
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-28 05:43:25 -0500

    File updated at 12/28/2025, 5:43:25 AM

--- 20-ValidParentheses/20-ValidParentheses.py
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -1,15 +1,16 @@
-# Last updated: 10/26/2025, 10:18:56 PM
-class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        close_open = {")":"(","]":"[","}":"{"}
-
-        for i in s:
-            if i in close_open:
-                if stack and stack[-1] == close_open[i]:
-                    stack.pop()
-                else:
-                    return False
-            else:
-                stack.append(i)
-        return True if not stack else False
+# Last updated: 12/28/2025, 5:43:25 AM
+1class Solution:
+2    def isValid(self, s: str) -> bool:
+3        stack = []
+4        close_open = {")":"(","]":"[","}":"{"}
+5
+6        for i in range(len(s)):
+7            if s[i] in close_open:
+8                if stack and stack[-1] == close_open[s[i]]:
+9                    stack.pop()
+10                else:
+11                    return False
+12            else:
+13                stack.append(s[i])
+14        return True if not stack else False
+15
