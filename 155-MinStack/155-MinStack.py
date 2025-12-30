e31f4f01c498d7502aa08611bed8d83aa4ec0a1d
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-30 18:22:18 -0500

    File updated at 12/30/2025, 6:22:18 PM

--- 155-MinStack/155-MinStack.py
+++ 155-MinStack/155-MinStack.py
@@ -1,4 +1,4 @@
-# Last updated: 12/28/2025, 5:50:24 AM
+# Last updated: 12/30/2025, 6:22:18 PM
 1class MinStack:
 2
 3    def __init__(self):
@@ -11,24 +11,23 @@
 10        val = min(val,self.minstack[-1]) if self.minstack else val
 11        self.minstack.append(val)
 12        
-13
-14    def pop(self) -> None:
-15        self.stack.pop()
-16        self.minstack.pop()
-17        
-18
-19    def top(self) -> int:
-20        return self.stack[-1]
-21        
-22
-23    def getMin(self) -> int:
-24        return self.minstack[-1]
-25        
+13    def pop(self) -> None:
+14        self.stack.pop()
+15        self.minstack.pop()
+16        
+17
+18    def top(self) -> int:
+19        return self.stack[-1]
+20        
+21
+22    def getMin(self) -> int:
+23        return self.minstack[-1]
+24        
+25
 26
-27
-28# Your MinStack object will be instantiated and called as such:
-29# obj = MinStack()
-30# obj.push(val)
-31# obj.pop()
-32# param_3 = obj.top()
-33# param_4 = obj.getMin()
+27# Your MinStack object will be instantiated and called as such:
+28# obj = MinStack()
+29# obj.push(val)
+30# obj.pop()
+31# param_3 = obj.top()
+32# param_4 = obj.getMin()
