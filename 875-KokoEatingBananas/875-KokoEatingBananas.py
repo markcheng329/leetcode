af8a11d1bc6b1105bdbcd239dfda5ebdb23d1243
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-31 03:55:58 -0500

    File updated at 12/31/2025, 3:55:58 AM

--- 875-KokoEatingBananas/875-KokoEatingBananas.py
+++ 875-KokoEatingBananas/875-KokoEatingBananas.py
@@ -1,15 +1,16 @@
-class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l,r = 1,max(piles)
-        res = r
-
-        while l <= r:
-            k = (l+r)//2
-            hours = sum(math.ceil(p/k) for p in piles)
-
-            if hours <= h:
-                res = min(res,k)
-                r = k-1
-            else:
-                l = k+1
-        return res
+# Last updated: 12/31/2025, 3:55:58 AM
+1class Solution:
+2    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+3        l, r = 1, max(piles)
+4        res = r
+5
+6        while l <= r:
+7            mid = (l+r)//2
+8            hours = sum(math.ceil(p/mid) for p in piles)
+9
+10            if hours <= h:
+11                res = min(res,mid)
+12                r = mid -1
+13            else:
+14                l = mid +1
+15        return res
