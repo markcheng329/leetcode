b07f33b8ffba4e0d21f619a2327235117bdc4e17
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2025-12-31 03:14:04 -0500

    File updated at 12/31/2025, 3:14:04 AM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
@@ -1,13 +1,15 @@
-# Last updated: 10/18/2025, 11:49:06 PM
-class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        l,r = 0,len(nums)-1
-
-        while l < r :
-            mid = (l+r)//2
-            if nums[mid] > nums[r]:
-                l = mid +1
-            else:
-                r = mid
-
-        return nums[l]
+# Last updated: 12/31/2025, 3:14:04 AM
+1class Solution:
+2    def findMin(self, nums: List[int]) -> int:
+3        l , r = 0,len(nums)-1
+4
+5        if nums[l] < nums[r]:
+6            return nums[l]
+7
+8        while l < r:
+9            mid = (l+r)//2
+10            if nums[mid] > nums[r]:
+11                l = mid +1
+12            else:
+13                r = mid
+14        return nums[l]
