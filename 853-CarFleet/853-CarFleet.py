f52790db5c274eab07ca83e1c01ac9b7201b6618
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-02 04:26:01 -0500

    File updated at 1/2/2026, 4:26:01 AM

--- 853-CarFleet/853-CarFleet.py
+++ 853-CarFleet/853-CarFleet.py
@@ -1,4 +1,4 @@
-# Last updated: 1/2/2026, 4:25:00 AM
+# Last updated: 1/2/2026, 4:26:01 AM
 1class Solution:
 2    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
 3        stack = []
@@ -7,7 +7,6 @@
 6
 7        for position,speed in groups:
 8            i = (target - position) / speed
-9            while stack and i > stack[-1]:
+9            while not stack or i > stack[-1]:
 10                stack.append(i)
-11            stack.append(i) if stack == [] else None
-12        return len(stack)
+11        return len(stack)
