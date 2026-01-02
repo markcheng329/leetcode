bf865081857bd1f25cbd23015dee2ab53e6f5b16
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-02 05:23:10 -0500

    File updated at 1/2/2026, 5:23:10 AM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
@@ -1,23 +1,22 @@
-# Last updated: 12/31/2025, 3:46:55 AM
+# Last updated: 1/2/2026, 5:23:10 AM
 1class Solution:
 2    def search(self, nums: List[int], target: int) -> int:
 3        l, r = 0,len(nums)-1
-4        
+4
 5        while l <= r:
 6            mid = (l+r)//2
-7
-8            if nums[mid] == target:
-9                return mid
-10            
-11            if nums[r] >= nums[mid]:
-12                if nums[r] >= target > nums[mid]:
-13                    l = mid +1
-14                else:
-15                    r = mid-1
-16            else:
-17                if nums[mid] > target >= nums[l]:
-18                    r = mid -1
-19                else:
-20                    l = mid +1
-21        return -1
-22
+7            if nums[mid] == target:
+8                return mid
+9
+10            if nums[mid] < nums[r]:
+11                if nums[r]>= target >nums[mid]:
+12                    l = mid +1
+13                else:
+14                    r = mid -1
+15            else:
+16                if nums[l] <= target < nums[mid]:
+17                    r = mid -1
+18                else:
+19                    l = mid +1
+20        return -1
+21
