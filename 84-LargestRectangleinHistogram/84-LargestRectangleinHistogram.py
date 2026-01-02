31cba9f74aa5bea3a4af0acac16cb056af55690f
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-02 04:30:53 -0500

    File updated at 1/2/2026, 4:30:52 AM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.py
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.py
@@ -1,4 +1,4 @@
-# Last updated: 12/30/2025, 7:05:25 PM
+# Last updated: 1/2/2026, 4:30:52 AM
 1class Solution:
 2    def largestRectangleArea(self, heights: List[int]) -> int:
 3        stack = []
@@ -12,11 +12,7 @@
 11                start = index
 12            stack.append([start,heights[i]])
 13
-14        for a,b in stack:
-15            res = max(res,(len(heights)-a)*b)
-16            
+14        for i, h in stack:
+15            res = max(res,(len(heights)-i)*h)
+16        
 17        return res
-18
-19
-20                
-21
