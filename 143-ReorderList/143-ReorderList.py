de81fa2d98c7fe8754e542ba28de0750a0aed937
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-02 06:34:28 -0500

    File updated at 1/2/2026, 6:34:28 AM

--- 143-ReorderList/143-ReorderList.py
+++ 143-ReorderList/143-ReorderList.py
@@ -1,30 +1,36 @@
-# Last updated: 10/16/2025, 11:51:47 PM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        slow,fast = head,head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        prev,cur = None,slow.next
-        slow.next = None
-        while cur:
-            tmp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = tmp
-        
-        first,second = head,prev
-        while second:
-            tmp1,tmp2 = first.next,second.next
-            first.next = second
-            second.next = tmp1
-            first,second= tmp1,tmp2
+# Last updated: 1/2/2026, 6:34:28 AM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def reorderList(self, head: Optional[ListNode]) -> None:
+8        """
+9        Do not return anything, modify head in-place instead.
+10        """
+11        slow,fast = head,head
+12        while fast and fast.next:
+13            slow = slow.next
+14            fast = fast.next.next
+15        
+16        prev,cur = None,slow.next
+17        slow.next = None
+18        while cur:
+19            nxt = cur.next
+20            cur.next = prev
+21            prev = cur
+22            cur = nxt
+23        
+24        first,second = head, prev
+25        while second:
+26            temp1 = first.next
+27            temp2 = second.next
+28            first.next = second
+29            second.next = temp1
+30            first = temp1
+31            second = temp2
+32
+33
+34
+35            
