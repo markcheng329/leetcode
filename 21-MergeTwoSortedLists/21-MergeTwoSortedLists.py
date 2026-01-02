3e627b12d0c0628b1a6710fdd76911356d2877b2
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-02 05:36:09 -0500

    File updated at 1/2/2026, 5:36:09 AM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -1,22 +1,24 @@
-# Last updated: 10/18/2025, 7:29:06 PM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        tail = dummy
-
-        while list1 and list2:
-            if list1.val > list2.val:
-                tail.next = list2
-                list2 = list2.next
-            else:
-                tail.next = list1
-                list1 = list1.next
-            tail = tail.next
-        tail.next = list1 or list2
-
-        return dummy.next
+# Last updated: 1/2/2026, 5:36:09 AM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+8        dummy = ListNode()
+9        tail = dummy
+10
+11        while list1 and list2:
+12            if list1.val > list2.val:
+13                tail.next = list2
+14                list2 = list2.next
+15            else:
+16                tail.next = list1
+17                list1 = list1.next
+18            tail = tail.next
+19        
+20        tail.next = list1 or list2
+21        return dummy.next
+22
+23
