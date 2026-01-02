8c936197a4bc4773331174ed13bb1a80845cbe14
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-02 05:30:44 -0500

    File updated at 1/2/2026, 5:30:44 AM

--- 206-ReverseLinkedList/206-ReverseLinkedList.py
+++ 206-ReverseLinkedList/206-ReverseLinkedList.py
@@ -1,16 +1,16 @@
-# Last updated: 10/18/2025, 7:27:15 PM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev,cur = None,head
-
-        while cur:
-            nxt = cur.next
-            cur.next = prev
-            prev = cur
-            cur = nxt
-        return prev
+# Last updated: 1/2/2026, 5:30:44 AM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+8        prev,cur = None,head
+9
+10        while cur:
+11            nxt = cur.next
+12            cur.next = prev
+13            prev = cur
+14            cur = nxt
+15        return prev
