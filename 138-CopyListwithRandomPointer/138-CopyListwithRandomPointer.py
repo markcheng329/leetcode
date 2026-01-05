a532055931e288dd8ae64cf92625be31741bbe8d
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-05 02:15:12 -0500

    File updated at 1/5/2026, 2:15:12 AM

--- 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.py
+++ 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.py
@@ -1,4 +1,4 @@
-# Last updated: 1/5/2026, 2:06:51 AM
+# Last updated: 1/5/2026, 2:15:12 AM
 1"""
 2# Definition for a Node.
 3class Node:
@@ -12,32 +12,28 @@
 11    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
 12        if not head:
 13            return None
-14
-15        #a-a'-b-b'-c-c'
-16        
-17        l1 = head
-18        while l1:
-19            l2 = Node(l1.val)
-20            l2.next = l1.next
-21            l1.next = l2
-22            l1 = l2.next
-23        
-24        l1 = head
-25        while l1:
-26            if l1.random:
-27                l1.next.random = l1.random.next
-28            l1 = l1.next.next
-29        
-30        newhead = head.next
-31
-32        l1= head
-33        while l1:
-34            l2 = l1.next
-35            l1.next = l2.next
-36            if l2.next:
-37                l2.next = l2.next.next
-38            l1 = l1.next
-39        
-40        return newhead
-41
-42
+14        
+15        l1 = head
+16        while l1:
+17            l2 = Node(l1.val)
+18            l2.next = l1.next
+19            l1. next = l2
+20            l1 = l2.next
+21        
+22        l1 = head
+23        while l1:
+24            if l1.random:
+25                l1.next.random = l1.random.next
+26            l1 = l1.next.next
+27        
+28        newhead = head.next
+29
+30        l1 = head
+31        while l1:
+32            l2 = l1.next
+33            l1.next = l2.next
+34            if l2.next:
+35                l2.next = l2.next.next
+36            l1 = l1.next
+37        
+38        return newhead
