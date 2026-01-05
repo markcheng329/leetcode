366a0c6bb051fa370e8a8f4d4e0fe7fe35de138b
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-04 20:02:51 -0500

    File updated at 1/4/2026, 8:02:51 PM

--- 853-CarFleet/853-CarFleet.py
+++ 853-CarFleet/853-CarFleet.py
@@ -1,12 +1,12 @@
-# Last updated: 1/2/2026, 4:27:29 AM
+# Last updated: 1/4/2026, 8:02:51 PM
 1class Solution:
 2    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
 3        stack = []
 4
-5        groups = sorted(zip(position,speed),reverse = True)
+5        groups = sorted(zip(position,speed), reverse = True)
 6
-7        for pos,spe in groups:
-8            i = (target - pos) / spe
-9            if not stack or i > stack[-1]:
+7        for p,s in groups:
+8            i = (target-p)/ s
+9            if not stack or stack[-1] < i:
 10                stack.append(i)
 11        return len(stack)
