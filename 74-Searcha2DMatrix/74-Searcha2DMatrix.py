92049e78ad91bdc0cae0afc83c658c82d719787d
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-04 20:57:25 -0500

    File updated at 1/4/2026, 8:57:25 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.py
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.py
@@ -1,15 +1,15 @@
-# Last updated: 1/2/2026, 4:37:11 AM
+# Last updated: 1/4/2026, 8:57:25 PM
 1class Solution:
 2    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 3        m = len(matrix)
 4        n = len(matrix[0])
-5        l,r = 0,m*n-1
-6
-7        while l <= r:
-8            mid = (l+r)//2
-9            row = mid // n
-10            col = mid % n
-11
+5
+6        l,r = 0,m*n-1
+7
+8        while l <= r:
+9            mid = (l+r)//2
+10            row = mid //n
+11            col = mid % n
 12            if matrix[row][col] == target:
 13                return True
 14            
