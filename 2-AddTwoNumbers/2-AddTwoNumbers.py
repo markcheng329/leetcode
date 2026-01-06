09ebd170f60d243be90b705ddcfac3073158e583
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-06 01:39:35 -0500

    File updated at 1/6/2026, 1:39:35 AM

--- 2-AddTwoNumbers/2-AddTwoNumbers.py
+++ 2-AddTwoNumbers/2-AddTwoNumbers.py
@@ -1,4 +1,4 @@
-# Last updated: 1/5/2026, 2:23:10 AM
+# Last updated: 1/6/2026, 1:39:35 AM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -13,18 +13,14 @@
 12        while l1 or l2 or carry:
 13            v1 = l1.val if l1 else 0
 14            v2 = l2.val if l2 else 0
-15
-16            val = v1+v2+carry
-17            
-18            carry = val // 10
-19            val = val % 10
-20
-21            tail.next = ListNode(val)
-22            tail = tail.next
-23
-24            l1 = l1.next if l1 else None
-25            l2 = l2.next if l2 else None
-26        return dummy.next
-27            
-28
-29
+15            val = v1+v2+carry
+16
+17            carry = val // 10
+18            val = val % 10
+19
+20            tail.next = ListNode(val)
+21            tail = tail.next
+22
+23            l1 = l1.next if l1 else None
+24            l2 = l2.next if l2 else None
+25        return dummy.next
