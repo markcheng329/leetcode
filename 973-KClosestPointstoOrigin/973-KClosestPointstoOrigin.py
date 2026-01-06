4334776db6662024ab12a6d728e42e6a203c0dec
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-06 05:41:18 -0500

    File updated at 1/6/2026, 5:41:18 AM

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
@@ -1,19 +1,18 @@
-# Last updated: 11/16/2025, 5:23:32 AM
-class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        minheap = []
-        for x,y in points:
-            dis = x**2 + y**2
-            minheap.append([dis,x,y])
-        
-        heapq.heapify(minheap)
-        res = []
-
-
-        while k > 0:
-            dis,x,y = heapq.heappop(minheap)
-            res.append([x,y])
-            k -=1
-        return res
-
-        
+# Last updated: 1/6/2026, 5:41:18 AM
+1class Solution:
+2    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+3        minheap = []
+4        res = []
+5
+6        for x,y in points:
+7            dis = x**2 + y**2
+8            minheap.append([dis,x,y])
+9        heapq.heapify(minheap)
+10
+11        while k > 0:
+12            dis,x,y = heapq.heappop(minheap)
+13            res.append([x,y])
+14            k-=1
+15        
+16        return res
+17
