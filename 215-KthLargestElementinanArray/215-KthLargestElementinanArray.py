78bc22aed195848f5c5647c478005e8a2c421400
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-06 06:01:31 -0500

    File updated at 1/6/2026, 6:01:31 AM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.py
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.py
@@ -1,11 +1,10 @@
-# Last updated: 1/6/2026, 5:56:48 AM
+# Last updated: 1/6/2026, 6:01:31 AM
 1class Solution:
 2    def findKthLargest(self, nums: List[int], k: int) -> int:
 3        minheap = (nums)
-4        
-5        heapq.heapify(minheap)
-6
-7        while len(minheap) > k:
-8            heapq.heappop(minheap)
-9        
-10        return minheap[0]
+4        heapq.heapify(minheap)
+5
+6        while len(minheap) > k:
+7            heapq.heappop(minheap)
+8        
+9        return minheap[0]
