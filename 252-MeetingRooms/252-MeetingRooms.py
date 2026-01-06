c1f06c966a6f9111637e167b45058b2b57dedbed
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-06 04:02:08 -0500

    File updated at 1/6/2026, 4:02:08 AM

--- 252-MeetingRooms/252-MeetingRooms.py
+++ 252-MeetingRooms/252-MeetingRooms.py
@@ -1,13 +1,16 @@
-# Last updated: 10/22/2025, 12:10:07 AM
-class Solution:
-    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort(key=lambda x:x[0])
-
-        prev_end = float("-inf")
-
-        for start,end in intervals:
-            if start<prev_end:
-                return False
-            else:
-                prev_end = end
-        return True
+# Last updated: 1/6/2026, 4:02:08 AM
+1class Solution:
+2    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+3        if not intervals:
+4            return True
+5
+6        intervals.sort(key=lambda x:x[0])
+7
+8        prev_end = intervals[0][1]
+9
+10        for start,end in intervals[1:]:
+11            if start < prev_end:
+12                return False
+13            else:
+14                prev_end = end
+15        return True
