fb435271c8feb47f337738c3a8aa21d03fa623ec
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-06 04:08:04 -0500

    File updated at 1/6/2026, 4:08:04 AM

--- 253-MeetingRoomsII/253-MeetingRoomsII.py
+++ 253-MeetingRoomsII/253-MeetingRoomsII.py
@@ -1,13 +1,13 @@
-# Last updated: 11/19/2025, 2:15:24 PM
-class Solution:
-    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x:x[0])
-        minheap = []
-        res = 0
-
-        for start,end in intervals:
-            while minheap and minheap[0] <= start:
-                heapq.heappop(minheap)
-            heapq.heappush(minheap,end)
-            res = max(res,len(minheap))
-        return res
+# Last updated: 1/6/2026, 4:08:04 AM
+1class Solution:
+2    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+3        intervals.sort(key=lambda x:x[0])
+4        res = 0
+5        minheap = []
+6
+7        for start,end in intervals:
+8            while minheap and minheap[0] <= start:
+9                heapq.heappop(minheap)
+10            heapq.heappush(minheap,end)
+11            res = max(res,len(minheap))
+12        return res
