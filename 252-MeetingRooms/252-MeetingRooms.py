760575882cd55892311018e658c16aa23abc0db0
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-08 02:41:09 -0500

    File updated at 1/8/2026, 2:41:09 AM

--- 252-MeetingRooms/252-MeetingRooms.py
+++ 252-MeetingRooms/252-MeetingRooms.py
@@ -1,4 +1,4 @@
-# Last updated: 1/8/2026, 2:40:28 AM
+# Last updated: 1/8/2026, 2:41:09 AM
 1class Solution:
 2    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
 3        if not intervals:
@@ -6,13 +6,14 @@
 5
 6        intervals.sort(key=lambda x:x[0])
 7
-8        prev_end = intervals[0][1]
-9
+8        
+9        prev_end = intervals[0][1]
 10
-11        for s,e in intervals[1:]:
-12            if s < prev_end:
-13                return False
-14            else:
-15                prev_end = e
-16        return True
-17
+11        for start,end in intervals[1:]:
+12  
+13            if start < prev_end:
+14                return False
+15            else:
+16                # start >= prev_end
+17                prev_end = end
+18        return True
