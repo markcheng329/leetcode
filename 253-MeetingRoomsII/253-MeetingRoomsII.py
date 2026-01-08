79910457351c16bad4c44fa6dd46102f7ccb1446
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-08 02:50:15 -0500

    File updated at 1/8/2026, 2:50:15 AM

--- 253-MeetingRoomsII/253-MeetingRoomsII.py
+++ 253-MeetingRoomsII/253-MeetingRoomsII.py
@@ -1,4 +1,4 @@
-# Last updated: 1/8/2026, 2:49:51 AM
+# Last updated: 1/8/2026, 2:50:15 AM
 1class Solution:
 2    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
 3        minheap = []
@@ -8,7 +8,7 @@
 7        res = 0
 8
 9        for start,end in intervals:
-10            if minheap and start >= minheap[0] :
+10            if minheap and start >= minheap[0]:
 11                heapq.heappop(minheap)
 12            heapq.heappush(minheap,end)
 13        return len(minheap)
