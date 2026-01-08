f9dc3b0ac645e57652fe7cabfc9ec7116b5f4187
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-08 04:03:51 -0500

    File updated at 1/8/2026, 4:03:50 AM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,15 +1,16 @@
-# Last updated: 9/30/2025, 8:43:31 PM
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        res= 0
-        cur_end = 0
-        farest =0
-
-        for i in range(len(nums)-1):
-            farest = max(farest,i+nums[i])
-            if i == cur_end:
-                res +=1
-                cur_end = farest
-                if cur_end >= len(nums)-1:
-                    break
-        return res
+# Last updated: 1/8/2026, 4:03:50 AM
+1class Solution:
+2    def jump(self, nums: List[int]) -> int:
+3        cur_end = 0
+4        res = 0
+5        farthest = 0
+6
+7        for i in range(len(nums)-1):
+8            farthest = max(farthest,i+nums[i])
+9            if i == cur_end:
+10                res +=1
+11                cur_end = farthest
+12            
+13            if cur_end >= len(nums)-1:
+14                break
+15        return res
