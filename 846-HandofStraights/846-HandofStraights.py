89d6bea1e7d07821939f2234154769a01f52c26b
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-08 04:21:21 -0500

    Create 846-HandofStraights

---
+++ 846-HandofStraights/846-HandofStraights.py
@@ -0,0 +1,16 @@
+# Last updated: 1/8/2026, 4:21:21 AM
+1class Solution:
+2    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+3        if len(hand) % groupSize != 0:
+4            return False
+5        
+6        count = Counter(hand)
+7
+8        for num in sorted(hand):
+9            if count[num]:
+10                for i in range(num,num+groupSize):
+11                    if not count[i]:
+12                        return False
+13                    else:
+14                        count[i] -=1
+15        return True
