25d0c95f3c17db06b9edd8ead042bb0276c86340
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-09 03:52:04 -0500

    File updated at 1/9/2026, 3:52:04 AM

--- 53-MaximumSubarray/53-MaximumSubarray.py
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -1,12 +1,12 @@
-# Last updated: 1/8/2026, 3:36:10 AM
+# Last updated: 1/9/2026, 3:52:04 AM
 1class Solution:
 2    def maxSubArray(self, nums: List[int]) -> int:
-3        curres = 0
+3        curSum = 0
 4        res = nums[0]
 5
 6        for num in nums:
-7            if curres < 0:
-8                curres = 0
-9            curres += num
-10            res = max(res,curres)
+7            if curSum < 0:
+8                curSum = 0
+9            curSum += num
+10            res = max(res,curSum)
 11        return res
