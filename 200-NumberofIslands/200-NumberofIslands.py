ddc0e9e6d2cd61501ad6fabc6666b13d6d0c9c9f
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-10 00:06:31 -0500

    File updated at 1/10/2026, 12:06:31 AM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,24 +1,24 @@
-# Last updated: 1/8/2026, 6:02:36 AM
+# Last updated: 1/10/2026, 12:06:31 AM
 1class Solution:
 2    def numIslands(self, grid: List[List[str]]) -> int:
-3        rows = len(grid)
-4        cols = len(grid[0])
-5        res = 0
-6
-7        def dfs(r,c):
-8            if r < 0 or c < 0 or r>= rows or c>=cols or grid[r][c] == "0":
-9                return 
-10            
-11            grid[r][c] = "0"
-12            dfs(r+1,c)
-13            dfs(r-1,c)
-14            dfs(r,c+1)
-15            dfs(r,c-1)
-16
-17        for r in range(rows):
-18            for c in range(cols):
-19                if grid[r][c] == "1":
-20                    dfs(r,c)
-21                    res +=1
-22        return res
-23
+3        directions = [1,0],[-1,0],[0,1],[0,-1]
+4        res = 0
+5        rows = len(grid)
+6        cols = len(grid[0])
+7
+8        def dfs(r,c):
+9            if r < 0 or c<0 or r>=rows or c>=cols or grid[r][c] == "0":
+10                return
+11            
+12            grid[r][c] = "0"
+13            dfs(r+1,c)
+14            dfs(r-1,c)
+15            dfs(r,c+1)
+16            dfs(r,c-1)
+17        
+18        for r in range(rows):
+19            for c in range(cols):
+20                if grid[r][c] == "1":
+21                    dfs(r,c)
+22                    res +=1
+23        return res
