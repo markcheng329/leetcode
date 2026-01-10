1455a0528497ca473389f3d9ff00557d7c010d99
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-10 00:24:25 -0500

    File updated at 1/10/2026, 12:24:25 AM

--- 136-SingleNumber/136-SingleNumber.py
+++ 136-SingleNumber/136-SingleNumber.py
@@ -1,6 +1,11 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        res = 0 
-        for num in nums:
-            res = res ^ num
-        return res
+# Last updated: 1/10/2026, 12:24:25 AM
+1class Solution:
+2    def singleNumber(self, nums: List[int]) -> int:
+3        seen = set()
+4
+5        for num in nums:
+6            if num in seen:
+7                seen.remove(num)
+8            else:
+9                seen.add(num)
+10        return list(seen)[0]
