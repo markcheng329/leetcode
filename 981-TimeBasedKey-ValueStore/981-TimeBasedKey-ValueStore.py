a5e604110e3da5540b9209208750967b69584084
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-10 01:01:38 -0500

    File updated at 1/10/2026, 1:01:38 AM

--- 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
+++ 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
@@ -1,4 +1,4 @@
-# Last updated: 1/10/2026, 12:55:38 AM
+# Last updated: 1/10/2026, 1:01:38 AM
 1class TimeMap:
 2
 3    def __init__(self):
@@ -14,21 +14,18 @@
 13    def get(self, key: str, timestamp: int) -> str:
 14        res = ""
 15        values = self.keystore.get(key,[])
-16        l,r = 0,len(values)-1
+16        l,r = 0, len(values)-1
 17        while l <= r:
 18            mid = (l+r)//2
 19            if values[mid][1] <= timestamp:
 20                res = values[mid][0]
-21                l = mid + 1
+21                l = mid +1
 22            else:
-23                r = mid -1
+23                r = mid-1
 24        return res
 25
 26
-27        
-28
-29
-30# Your TimeMap object will be instantiated and called as such:
-31# obj = TimeMap()
-32# obj.set(key,value,timestamp)
-33# param_2 = obj.get(key,timestamp)
+27# Your TimeMap object will be instantiated and called as such:
+28# obj = TimeMap()
+29# obj.set(key,value,timestamp)
+30# param_2 = obj.get(key,timestamp)
