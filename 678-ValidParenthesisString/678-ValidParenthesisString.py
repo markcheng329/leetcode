696ca278da6f3f910ffdc24c828ff0e6c7d8ce19
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-11 03:59:49 -0500

    File updated at 1/11/2026, 3:59:49 AM

--- 678-ValidParenthesisString/678-ValidParenthesisString.py
+++ 678-ValidParenthesisString/678-ValidParenthesisString.py
@@ -1,23 +1,23 @@
-# Last updated: 1/9/2026, 11:18:54 PM
+# Last updated: 1/11/2026, 3:59:49 AM
 1class Solution:
 2    def checkValidString(self, s: str) -> bool:
-3        lmin,lmax = 0,0
-4
-5        for i in range(len(s)):
-6            if s[i] == "(":
-7                lmin +=1
-8                lmax +=1
-9            elif s[i] == ")":
-10                lmin -=1
-11                lmax -=1
-12            else:
-13                lmin -=1
-14                lmax +=1
-15        
-16            if lmin < 0:
-17                lmin = 0
-18            
-19            if lmax < 0:
-20                return False
-21                
-22        return True if lmin == 0 else False
+3        lpmin = 0
+4        lpmax = 0
+5
+6        for i in range(len(s)):
+7            if s[i] == "(":
+8                lpmin +=1
+9                lpmax +=1
+10            elif s[i] == ")":
+11                lpmin -=1
+12                lpmax -=1
+13            else:
+14                lpmin -=1
+15                lpmax +=1
+16            
+17            if lpmin < 0:
+18                lpmin = 0
+19            
+20            if lpmax < 0:
+21                return False
+22        return True if lpmin==0 else False
