4ed331d1dee7c04de06e2cbfe9c5880c2ae39516
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-11 03:52:57 -0500

    File updated at 1/11/2026, 3:52:57 AM

--- 1899-MergeTripletstoFormTargetTriplet/1899-MergeTripletstoFormTargetTriplet.py
+++ 1899-MergeTripletstoFormTargetTriplet/1899-MergeTripletstoFormTargetTriplet.py
@@ -1,18 +1,17 @@
-# Last updated: 1/9/2026, 11:07:41 PM
+# Last updated: 1/11/2026, 3:52:57 AM
 1class Solution:
 2    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-3        answers = set()
-4
+3        seen = set()
+4        
 5        for t in triplets:
 6            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
 7                continue
 8            
 9            if t[0] == target[0]:
-10                answers.add(0)
+10                seen.add(0)
 11            if t[1] == target[1]:
-12                answers.add(1)
+12                seen.add(1)
 13            if t[2] == target[2]:
-14                answers.add(2)
-15
-16        return True if len(answers) ==3 else False
-17            
+14                seen.add(2)
+15        return True if len(seen) == 3 else False
+16            
