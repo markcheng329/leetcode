e79ae66ac4aca421e070bb0a070939a7d073d335
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-12 16:55:40 -0500

    File updated at 1/12/2026, 4:55:40 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,18 +1,17 @@
-# Last updated: 11/13/2025, 5:27:01 AM
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        subset = []
-
-        def dfs(i):
-            if i == len(nums):
-                res.append(subset.copy())
-                return
-            
-            subset.append(nums[i])
-            dfs(i+1)
-            subset.pop()
-            dfs(i+1)
-
-        dfs(0)
-        return res
+# Last updated: 1/12/2026, 4:55:40 PM
+1class Solution:
+2    def subsets(self, nums: List[int]) -> List[List[int]]:
+3        res = []
+4        subset = []
+5
+6        def dfs(i):
+7            if i == len(nums):
+8                res.append(subset.copy())
+9                return res
+10            
+11            subset.append(nums[i])
+12            dfs(i+1)
+13            subset.pop()
+14            dfs(i+1)
+15        dfs(0)
+16        return res
