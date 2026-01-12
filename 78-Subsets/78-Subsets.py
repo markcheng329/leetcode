473941ee6ece8e3e1c1b3f38217a2ad7b9ef57bd
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-12 17:18:23 -0500

    File updated at 1/12/2026, 5:18:23 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,8 +1,8 @@
-# Last updated: 1/12/2026, 4:55:40 PM
+# Last updated: 1/12/2026, 5:18:23 PM
 1class Solution:
 2    def subsets(self, nums: List[int]) -> List[List[int]]:
-3        res = []
-4        subset = []
+3        subset = []
+4        res = []
 5
 6        def dfs(i):
 7            if i == len(nums):
@@ -13,5 +13,6 @@
 12            dfs(i+1)
 13            subset.pop()
 14            dfs(i+1)
-15        dfs(0)
-16        return res
+15        
+16        dfs(0)
+17        return res
