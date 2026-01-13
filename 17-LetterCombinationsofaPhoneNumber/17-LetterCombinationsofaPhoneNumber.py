669b10ccbc1641ff29215e60c94793ac040ea622
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-13 01:19:57 -0500

    File updated at 1/13/2026, 1:19:57 AM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
@@ -1,4 +1,4 @@
-# Last updated: 1/13/2026, 1:16:11 AM
+# Last updated: 1/13/2026, 1:19:57 AM
 1class Solution:
 2    def letterCombinations(self, digits: str) -> List[str]:
 3        res = []
@@ -14,16 +14,17 @@
 13            "8": "tuv",
 14            "9": "wxyz",
 15        }
-16
+16        
 17        def dfs(i):
 18            if i == len(digits):
 19                res.append("".join(subset))
-20                return 
+20                return
 21            
 22            for ch in phone[digits[i]]:
 23                subset.append(ch)
 24                dfs(i+1)
 25                subset.pop()
-26
+26        
 27        dfs(0)
 28        return res
+29
