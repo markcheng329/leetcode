590f2c75bda90fa3bbd6da7a8ba812f36ae9620e
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-12 23:14:55 -0500

    File updated at 1/12/2026, 11:14:55 PM

--- 90-SubsetsII/90-SubsetsII.py
+++ 90-SubsetsII/90-SubsetsII.py
@@ -1,20 +1,18 @@
-# Last updated: 11/13/2025, 6:18:27 AM
-class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        res = []
-        subset = []
-
-        def backtracking(start):
-            res.append(subset.copy())
-
-            for i in range(start,len(nums)):
-                if i > start and nums[i] == nums[i-1]:
-                    continue
-            
-                subset.append(nums[i])
-                backtracking(i+1)
-                subset.pop()
-        
-        backtracking(0)
-        return res
+# Last updated: 1/12/2026, 11:14:55 PM
+1class Solution:
+2    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+3        nums.sort()
+4        res = []
+5        subset = []
+6
+7        def dfs(start):
+8            res.append(subset.copy())
+9
+10            for i in range(start,len(nums)):
+11                if i > start and nums[i] == nums[i-1]:
+12                    continue
+13                subset.append(nums[i])
+14                dfs(i+1)
+15                subset.pop()
+16        dfs(0)
+17        return res
