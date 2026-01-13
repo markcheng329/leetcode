5fdcb6a432d227bc03de7f80fbd00f64043d7c99
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-13 01:29:21 -0500

    File updated at 1/13/2026, 1:29:20 AM

--- 51-N-Queens/51-N-Queens.py
+++ 51-N-Queens/51-N-Queens.py
@@ -1,34 +1,34 @@
-# Last updated: 11/14/2025, 5:54:45 PM
-class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
-        col = set()
-        posdiag = set()
-        nagdiag = set()
-
-        res= []
-        board = [ ["."] * n for i in range(n)]
-
-        def backtrack(r):
-            if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
-                return
-            
-            for c in range(n):
-                if c in col or (r+c) in posdiag or (r-c) in nagdiag:
-                    continue
-            
-                col.add(c)
-                posdiag.add(r+c)
-                nagdiag.add(r-c)
-                board[r][c] = "Q"
-
-                backtrack(r+1)
-
-                col.remove(c)
-                posdiag.remove(r+c)
-                nagdiag.remove(r-c)
-                board[r][c] = "."
-        
-        backtrack(0)
-        return res
+# Last updated: 1/13/2026, 1:29:20 AM
+1class Solution:
+2    def solveNQueens(self, n: int) -> List[List[str]]:
+3        col = set()
+4        posdiag = set()
+5        nagdiag = set()
+6        res= []
+7        board = [ ["."] * n for i in range(n)]
+8
+9        def dfs(r):
+10            if r == n:
+11                subset = ["".join(r) for r in board]
+12                res.append(subset)
+13                return
+14            
+15            for c in range(n):
+16                if c in col or (r+c) in posdiag or (r-c) in nagdiag:
+17                    continue
+18            
+19                col.add(c)
+20                posdiag.add(r+c)
+21                nagdiag.add(r-c)
+22                board[r][c] = "Q"
+23
+24                dfs(r+1)
+25
+26                col.remove(c)
+27                posdiag.remove(r+c)
+28                nagdiag.remove(r-c)
+29                board[r][c] = "."
+30        
+31        dfs(0)
+32        return res
+33
