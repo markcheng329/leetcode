be9d9f7462359b34a47e516be3a3290e25eb468c
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-13 00:16:52 -0500

    File updated at 1/13/2026, 12:16:51 AM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,23 +1,23 @@
-# Last updated: 11/13/2025, 4:57:46 AM
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        rows,cols = len(board),len(board[0])
-
-        def dfs(r,c,i):
-            if i == len(word):
-                return True
-            
-            if (r not in range(rows) or c not in range(cols) or word[i] != board[r][c] or board[r][c] == "#"):
-                return False
-            
-            board[r][c] = "#"
-            res = dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1)
-            board[r][c] = word[i]
-            return res
-        
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r,c,0) == True:
-                    return True
-        return False
-                
+# Last updated: 1/13/2026, 12:16:51 AM
+1class Solution:
+2    def exist(self, board: List[List[str]], word: str) -> bool:
+3        rows,cols = len(board),len(board[0])
+4
+5        def dfs(r,c,i):
+6            if i == len(word):
+7                return True
+8            
+9            if r < 0 or c< 0 or r >= rows or c >= cols or board[r][c] != word[i] or board[r][c] == "#":
+10                return False
+11            
+12            board[r][c] = "#"
+13            res = dfs(r+1,c,i+1) or dfs(r,c+1,i+1) or dfs(r-1,c,i+1) or dfs(r,c-1,i+1)
+14            board[r][c] = word[i]
+15            return res
+16
+17
+18        for r in range(rows):
+19            for c in range(cols):
+20                if dfs(r,c,0) == True:
+21                    return True
+22        return False
