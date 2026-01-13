e2364a7a7a62d98a065297a8d6019931a1f8a418
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-12 23:27:17 -0500

    File updated at 1/12/2026, 11:27:16 PM

--- 22-GenerateParentheses/22-GenerateParentheses.py
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -1,23 +1,23 @@
-# Last updated: 11/13/2025, 6:54:03 AM
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        subset = []
-
-        def backtracking(left,right):
-            if len(subset) == 2 * n:
-                res.append("".join(subset))
-                return 
-            
-            if left < n:
-                subset.append("(")
-                backtracking(left+1,right)
-                subset.pop()
-            
-            if right < left:
-                subset.append(")")
-                backtracking(left,right+1)
-                subset.pop()
-        
-        backtracking(0,0)
-        return res
+# Last updated: 1/12/2026, 11:27:16 PM
+1class Solution:
+2    def generateParenthesis(self, n: int) -> List[str]:
+3        res = []
+4        subset = []
+5
+6        def dfs(l,r):
+7            if len(subset) == 2*n:
+8                res.append("".join(subset.copy()))
+9                return
+10            
+11            if l < n:
+12                subset.append("(")
+13                dfs(l+1,r)
+14                subset.pop()
+15            
+16            if r < l:
+17                subset.append(")")
+18                dfs(l,r+1)
+19                subset.pop()
+20        
+21        dfs(0,0)
+22        return res
