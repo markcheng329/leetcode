8b486d7f6e5df4c8a00bf16ac976229a3a3ee1a8
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-13 21:14:22 -0500

    File updated at 1/13/2026, 9:14:22 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,12 +1,12 @@
-# Last updated: 1/12/2026, 6:57:43 PM
+# Last updated: 1/13/2026, 9:14:22 PM
 1class Solution:
 2    def subsets(self, nums: List[int]) -> List[List[int]]:
-3        subset = []
-4        res = []
+3        res = []
+4        subset = []
 5
 6        def dfs(start):
 7            res.append(subset.copy())
-8
+8            
 9            for i in range(start,len(nums)):
 10                subset.append(nums[i])
 11                dfs(i+1)
