f49907700858826187f889b20c4f1e8609e2821a
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-18 00:15:33 -0500

    File updated at 1/18/2026, 12:15:33 AM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,30 +1,28 @@
-# Last updated: 1/17/2026, 11:47:41 PM
+# Last updated: 1/18/2026, 12:15:33 AM
 1class Solution:
 2    def orangesRotting(self, grid: List[List[int]]) -> int:
 3        rows = len(grid)
 4        cols = len(grid[0])
 5        fresh = 0
 6        res = 0
-7        directions = [1,0],[-1,0],[0,1],[0,-1]
-8        q = deque()
-9
-10        for r in range(rows):
-11            for c in range(cols):
-12                if grid[r][c] == 1:
-13                    fresh +=1
-14                elif grid[r][c] == 2:
-15                    q.append((r,c))
-16                
-17        while fresh > 0 and q:
-18            for i in range(len(q)):
-19                r,c = q.popleft()
-20                for dr,dc in directions:
-21                    nr,nc = r+dr,c + dc
-22                    if nr in range(rows) and nc in range(cols) and grid[nr][nc] == 1:
-23                        grid[nr][nc] = 2
-24                        fresh -=1
-25                        q.append((nr,nc))
-26            res +=1
-27        
-28        return res if fresh == 0 else -1
-29
+7        q = deque()
+8
+9        for r in range(rows):
+10            for c in range(cols):
+11                if grid[r][c] == 1:
+12                    fresh +=1
+13                elif grid[r][c] == 2:
+14                    q.append((r,c))
+15        
+16        while fresh > 0 and q:
+17            for i in range(len(q)):
+18                r,c = q.popleft()
+19                for dr,dc in [1,0],[-1,0],[0,1],[0,-1]:
+20                    nr,nc = r+dr,c+dc
+21                    if nr in range(rows) and nc in range(cols) and grid[nr][nc] == 1:
+22                        grid[nr][nc] = 2
+23                        fresh -=1
+24                        q.append((nr,nc))
+25            res +=1
+26        
+27        return res if fresh == 0 else -1
