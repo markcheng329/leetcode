c3840b26e8f014b3d3ef14e3d47a467b8b21cbf3
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-18 04:59:52 -0500

    File updated at 1/18/2026, 4:59:51 AM

--- 210-CourseScheduleII/210-CourseScheduleII.py
+++ 210-CourseScheduleII/210-CourseScheduleII.py
@@ -1,33 +1,32 @@
-# Last updated: 11/13/2025, 3:25:45 AM
-class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        preMap = [[] for i in range(numCourses)]
-        for crs,pre in prerequisites:
-            preMap[crs].append(pre)
-        
-        cycle = set()
-        visit = set()
-        res = []
-
-        def dfs(crs):
-            if crs in cycle:
-                return False
-            
-            if crs in visit:
-                return True
-            
-            cycle.add(crs)
-            for pre in preMap[crs]:
-                if not dfs(pre):
-                    return False
-            cycle.remove(crs)
-            visit.add(crs)
-            preMap[crs] = []
-            res.append(crs)
-            return True
-        
-        for i in range(numCourses):
-            if not dfs(i):
-                return []
-        return res
-
+# Last updated: 1/18/2026, 4:59:51 AM
+1class Solution:
+2    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+3        preMap = [[] for i in range(numCourses)]
+4
+5        for crs,pre in prerequisites:
+6            preMap[crs].append(pre)
+7        
+8        visit,cycle = set(),set()
+9        res = []
+10
+11        def dfs(crs):
+12            if crs in cycle:
+13                return False
+14            
+15            if crs in visit:
+16                return True
+17            
+18            cycle.add(crs)
+19            for pre in preMap[crs]:
+20                if not dfs(pre):
+21                    return False
+22            cycle.remove(crs)
+23            visit.add(crs)
+24            res.append(crs)
+25            return True
+26        
+27        for i in range(numCourses):
+28            if not dfs(i):
+29                return []
+30        return res
+31        
