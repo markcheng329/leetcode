474daab226e82d894c40acf59528c0fea8fc1540
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-18 03:56:56 -0500

    File updated at 1/18/2026, 3:56:56 AM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -1,30 +1,32 @@
-# Last updated: 11/13/2025, 2:12:45 AM
-class Solution:
-    def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        rows,cols = len(board),len(board[0])
-        directions = [1,0],[-1,0],[0,1],[0,-1]
-
-        def dfs(r,c):
-            if (r not in range(rows) or c not in range(cols) or board[r][c] != "O"):
-                return
-            board[r][c] = "#"
-            for dr,dc in directions:
-                dfs(r+dr,c+dc)
-        
-        for r in range(rows):
-            dfs(r,0)
-            dfs(r,cols-1)
-        
-        for c in range(cols):
-            dfs(0,c)
-            dfs(rows-1,c)
-        
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                if board[r][c] == "#":
-                    board[r][c] = "O"
+# Last updated: 1/18/2026, 3:56:56 AM
+1class Solution:
+2    def solve(self, board: List[List[str]]) -> None:
+3        """
+4        Do not return anything, modify board in-place instead.
+5        """
+6        rows,cols = len(board),len(board[0])
+7        directions = [1,0],[-1,0],[0,1],[0,-1]
+8        
+9        def dfs(r,c):
+10            if r not in range(rows) or c not in range(cols) or board[r][c] != "O":
+11                return
+12            
+13            board[r][c] = "#"
+14            for dr,dc in directions:
+15                dfs(r+dr,c+dc)
+16        
+17        for r in range(rows):
+18            dfs(r,0)
+19            dfs(r,cols-1)
+20        
+21        for c in range(cols):
+22            dfs(0,c)
+23            dfs(rows-1,c)
+24        
+25        for r in range(rows):
+26            for c in range(cols):
+27                if board[r][c] == "O":
+28                    board[r][c] = "X"
+29                if board[r][c] == "#":
+30                    board[r][c] = "O"
+31                
