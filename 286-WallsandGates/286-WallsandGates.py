515175ba75af1f3d77034dbf0f37cf8cd175fc8a
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-18 03:29:21 -0500

    File updated at 1/18/2026, 3:29:20 AM

--- 286-WallsandGates/286-WallsandGates.py
+++ 286-WallsandGates/286-WallsandGates.py
@@ -1,23 +1,22 @@
-# Last updated: 11/12/2025, 8:23:47 PM
-class Solution:
-    def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        """
-        Do not return anything, modify rooms in-place instead.
-        """
-        inf = 2**31-1
-        rows,cols = len(rooms),len(rooms[0])
-        q =deque()
-
-        for r in range(rows):
-            for c in range(cols):
-                if rooms[r][c] == 0:
-                    q.append((r,c))
-        
-        while q:
-            r,c = q.popleft()
-            for dr,dc in [1,0],[-1,0],[0,1],[0,-1]:
-                nr,nc = r+dr,c+dc
-                if (nr in range(rows) and nc in range(cols) and rooms[nr][nc] == inf):
-                    rooms[nr][nc] = rooms[r][c] +1
-                    q.append((nr,nc))
-
+# Last updated: 1/18/2026, 3:29:20 AM
+1class Solution:
+2    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+3        """
+4        Do not return anything, modify rooms in-place instead.
+5        """
+6        inf = 2**31 - 1
+7        rows,cols = len(rooms),len(rooms[0])
+8        q = deque()
+9
+10        for r in range(rows):
+11            for c in range(cols):
+12                if rooms[r][c] == 0:
+13                    q.append((r,c))
+14        
+15        while q:
+16            r,c = q.popleft()
+17            for dr,dc in [1,0],[-1,0],[0,1],[0,-1]:
+18                nr,nc = r+dr,c+dc
+19                if nr in range(rows) and nc in range(cols) and rooms[nr][nc] == inf:
+20                    rooms[nr][nc] = rooms[r][c] + 1
+21                    q.append((nr,nc))
