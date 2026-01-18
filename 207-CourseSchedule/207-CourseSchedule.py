94d57733027a44c065310d75cd3d9e891178de1e
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-18 04:54:51 -0500

    File updated at 1/18/2026, 4:54:51 AM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,28 +1,27 @@
-# Last updated: 1/18/2026, 4:50:33 AM
+# Last updated: 1/18/2026, 4:54:51 AM
 1class Solution:
 2    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 3        preMap = [[] for i in range(numCourses)]
 4
 5        for crs,pre in prerequisites:
 6            preMap[crs].append(pre)
-7
+7        
 8        visit = set()
 9
 10        def dfs(crs):
 11            if crs in visit:
 12                return False
-13            
+13
 14            visit.add(crs)
 15            for pre in preMap[crs]:
-16                if dfs(pre) != True:
+16                if not dfs(pre):
 17                    return False
-18            
+18                
 19            visit.remove(crs)
 20            preMap[crs] = []
 21            return True
 22        
 23        for i in range(numCourses):
-24            if dfs(i) != True:
+24            if not dfs(i):
 25                return False
 26        return True
-27        
