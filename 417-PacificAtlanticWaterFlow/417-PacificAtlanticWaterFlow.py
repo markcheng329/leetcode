0fb6868c229b30283ad7842e5f43194308959c91
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-18 01:28:01 -0500

    File updated at 1/18/2026, 1:28:01 AM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,31 +1,32 @@
-# Last updated: 11/13/2025, 1:57:54 AM
-class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows,cols = len(heights),len(heights[0])
-        pac,atl = set(),set()
-        res = []
-        directions = [1,0],[-1,0],[0,1],[0,-1]
-
-        def dfs(r,c,visited,prevheight):
-            if (r not in range(rows) or c not in range(cols) or (r,c) in visited or heights[r][c] < prevheight):
-                return
-        
-            visited.add((r,c))
-            for dr,dc in directions:
-                dfs(r+dr,c+dc,visited,heights[r][c])
-            
-        for r in range(rows):
-            dfs(r,0,pac,heights[r][0])
-            dfs(r,cols-1,atl,heights[r][cols-1])
-        
-        for c in range(cols):
-            dfs(0,c,pac,heights[0][c])
-            dfs(rows-1,c,atl,heights[rows-1][c])
-        
-        for r in range(rows):
-            for c in range(cols):
-                if (r,c) in pac and (r,c) in atl:
-                    res.append((r,c))
-        return res
-
-            
+# Last updated: 1/18/2026, 1:28:01 AM
+1class Solution:
+2    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+3        rows = len(heights)
+4        cols = len(heights[0])
+5        res = []
+6        pac = set()
+7        atl = set()
+8        directions = [1,0],[-1,0],[0,1],[0,-1]
+9        
+10        def dfs(r,c,visited,prevHeight):
+11            if r not in range(rows) or c not in range(cols) or (r,c) in visited or heights[r][c] < prevHeight:
+12                return
+13            
+14            visited.add((r,c))
+15            for dr,dc in directions:
+16                dfs(r+dr,c+dc,visited,heights[r][c])
+17            
+18        for r in range(rows):
+19            dfs(r,0,pac,heights[r][0])
+20            dfs(r,cols-1,atl,heights[r][cols-1])
+21            
+22        for c in range(cols):
+23            dfs(0,c,pac,heights[0][c])
+24            dfs(rows-1,c,atl,heights[rows-1][c])
+25            
+26        for r in range(rows):
+27            for c in range(cols):
+28                if (r,c) in pac and (r,c) in atl:
+29                    res.append((r,c))
+30        return res
+31            
