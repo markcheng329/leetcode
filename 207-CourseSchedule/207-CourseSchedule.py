383fd7fb8657a0c63089cbbdae64754a09c3b3ea
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-18 04:50:33 -0500

    File updated at 1/18/2026, 4:50:33 AM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,26 +1,28 @@
-# Last updated: 1/18/2026, 4:47:03 AM
+# Last updated: 1/18/2026, 4:50:33 AM
 1class Solution:
 2    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 3        preMap = [[] for i in range(numCourses)]
-4        for crs,pre in prerequisites:
-5            preMap[crs].append(pre)
-6        visit = set()
+4
+5        for crs,pre in prerequisites:
+6            preMap[crs].append(pre)
 7
-8        def dfs(crs):
-9            if crs in visit:
-10                return False
-11            
-12            visit.add(crs)
-13            for pre in preMap[crs]:
-14                if dfs(pre) != True:
-15                    return False
-16                
-17            visit.remove(crs)
-18            preMap[crs] = []
-19            return True
-20        
-21        for i in range(numCourses):
-22            if not dfs(i):
-23                return False
-24        return True
-25
+8        visit = set()
+9
+10        def dfs(crs):
+11            if crs in visit:
+12                return False
+13            
+14            visit.add(crs)
+15            for pre in preMap[crs]:
+16                if dfs(pre) != True:
+17                    return False
+18            
+19            visit.remove(crs)
+20            preMap[crs] = []
+21            return True
+22        
+23        for i in range(numCourses):
+24            if dfs(i) != True:
+25                return False
+26        return True
+27        
