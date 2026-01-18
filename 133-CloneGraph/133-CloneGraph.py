af0f8de04e2483ec3ee228628f507c462e1de577
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-18 06:45:11 -0500

    File updated at 1/18/2026, 6:45:10 AM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,28 +1,29 @@
-# Last updated: 11/12/2025, 5:06:27 PM
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
-from typing import Optional
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return None
-        
-        oldtonew = {}
-
-        def dfs(node):
-            if node in oldtonew:
-                return oldtonew[node]
-            
-            copy = Node(node.val)
-            oldtonew[node] = copy
-
-            for nei in node.neighbors:
-                copy.neighbors.append(dfs(nei))
-            return copy
-        return dfs(node) if not None else None
+# Last updated: 1/18/2026, 6:45:10 AM
+1"""
+2# Definition for a Node.
+3class Node:
+4    def __init__(self, val = 0, neighbors = None):
+5        self.val = val
+6        self.neighbors = neighbors if neighbors is not None else []
+7"""
+8
+9from typing import Optional
+10class Solution:
+11    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+12        if not node:
+13            return None
+14        
+15        oldtonew = {}
+16
+17        def dfs(node):
+18            if node in oldtonew:
+19                return oldtonew[node]
+20            
+21            copy = Node(node.val)
+22            oldtonew[node] = copy
+23
+24            for nei in node.neighbors:
+25                copy.neighbors.append(dfs(nei))
+26            return copy
+27        
+28        return dfs(node)
