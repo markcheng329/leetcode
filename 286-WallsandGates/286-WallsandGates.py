6bc5601a4f05e8ee4c8739028ec841003b666cdd
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-18 03:39:00 -0500

    File updated at 1/18/2026, 3:39:00 AM

--- 286-WallsandGates/286-WallsandGates.py
+++ 286-WallsandGates/286-WallsandGates.py
@@ -1,22 +1,23 @@
-# Last updated: 1/18/2026, 3:29:20 AM
+# Last updated: 1/18/2026, 3:39:00 AM
 1class Solution:
 2    def wallsAndGates(self, rooms: List[List[int]]) -> None:
 3        """
 4        Do not return anything, modify rooms in-place instead.
 5        """
-6        inf = 2**31 - 1
+6        inf = 2**31 -1
 7        rows,cols = len(rooms),len(rooms[0])
 8        q = deque()
-9
-10        for r in range(rows):
-11            for c in range(cols):
-12                if rooms[r][c] == 0:
-13                    q.append((r,c))
-14        
-15        while q:
-16            r,c = q.popleft()
-17            for dr,dc in [1,0],[-1,0],[0,1],[0,-1]:
-18                nr,nc = r+dr,c+dc
-19                if nr in range(rows) and nc in range(cols) and rooms[nr][nc] == inf:
-20                    rooms[nr][nc] = rooms[r][c] + 1
-21                    q.append((nr,nc))
+9        directions = [1,0],[-1,0],[0,1],[0,-1]
+10
+11        for r in range(rows):
+12            for c in range(cols):
+13                if rooms[r][c] == 0:
+14                    q.append((r,c))
+15        
+16        while q:
+17            r,c = q.popleft()
+18            for dr,dc in directions:
+19                nr,nc = r + dr, c + dc
+20                if nr in range(rows) and nc in range(cols) and rooms[nr][nc] == inf:
+21                    rooms[nr][nc] = rooms[r][c] + 1
+22                    q.append((nr,nc))
