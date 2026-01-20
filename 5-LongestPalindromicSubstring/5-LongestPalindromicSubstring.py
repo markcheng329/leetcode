68b28f040748dd14b80343159462b8b8d02c32f1
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-20 03:59:35 -0500

    File updated at 1/20/2026, 3:59:34 AM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,25 +1,26 @@
-# Last updated: 11/13/2025, 4:46:19 AM
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        best_l,best_len = 0,0
-        
-        for i in range(len(s)):
-            l,r = i,i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r-l+1 > best_len:
-                    best_len = r-l+1
-                    best_l = l
-
-                l -=1
-                r+=1
-
-        for i in range(len(s)):
-            l,r = i,i+1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r-l+1 > best_len:
-                    best_len = r-l+1
-                    best_l = l              
-                l -=1
-                r+=1
-
-        return s[best_l:best_l + best_len]
+# Last updated: 1/20/2026, 3:59:34 AM
+1class Solution:
+2    def longestPalindrome(self, s: str) -> str:
+3        best_l,best_len = 0,0
+4
+5        for i in range(len(s)):
+6            l,r = i,i
+7            while l >= 0 and r < len(s) and s[l] == s[r]:
+8                if r-l + 1 > best_len:
+9                    best_len = r-l+1
+10                    best_l = l
+11            
+12                l-=1
+13                r+=1
+14    
+15        for i in range(len(s)):
+16            l,r = i,i+1
+17            while l >= 0 and r < len(s) and s[l] == s[r]:
+18                if r-l+1 > best_len:
+19                    best_len = r-l+1
+20                    best_l = l
+21                l-=1
+22                r+=1
+23        return s[best_l:best_l+best_len]
+24
+25
