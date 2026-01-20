a07fd3b712cfe17ae9a10933daa1b27dd9d8c863
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-20 03:25:35 -0500

    File updated at 1/20/2026, 3:25:35 AM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
@@ -1,10 +1,10 @@
-# Last updated: 11/13/2025, 4:29:37 AM
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
-        if len(cost) < 3:
-            return min(cost[0],cost[1])
-
-        for i in range(len(cost)-3,-1,-1):
-            cost[i] += min(cost[i+1],cost[i+2])
-        return min(cost[i],cost[i+1])
+# Last updated: 1/20/2026, 3:25:35 AM
+1class Solution:
+2    def minCostClimbingStairs(self, cost: List[int]) -> int:
+3        
+4        if len(cost) < 3:
+5            return min(cost[0],cost[1])
+6
+7        for i in range(len(cost)-3,-1,-1):
+8            cost[i] += min(cost[i+1],cost[i+2])
+9        return min(cost[0],cost[1])
