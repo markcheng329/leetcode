0d4c1b6402d1a1bec96088a55433049f95dd83e9
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-19 22:57:29 -0500

    File updated at 1/19/2026, 10:57:29 PM

--- 695-MaxAreaofIsland/695-MaxAreaofIsland.py
+++ 695-MaxAreaofIsland/695-MaxAreaofIsland.py
@@ -1,25 +1,22 @@
-# Last updated: 1/11/2026, 4:10:43 AM
+# Last updated: 1/19/2026, 10:57:29 PM
 1class Solution:
 2    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-3        res = 0
-4        rows = len(grid)
-5        cols = len(grid[0])
+3        rows,cols = len(grid),len(grid[0])
+4        directions = [1,0],[-1,0],[0,1],[0,-1]
+5        res = 0
 6
 7        def dfs(r,c):
-8            if r < 0 or c < 0 or r>=rows or c>=cols or grid[r][c] == 0:
+8            if r not in range(rows) or c not in range(cols) or grid[r][c] == 0:
 9                return 0
-10
-11            
-12            area = 1
-13            grid[r][c] = 0
-14            area += dfs(r+1,c)
-15            area += dfs(r-1,c)
-16            area += dfs(r,c+1)
-17            area += dfs(r,c-1)
-18            return area
-19        
-20        for r in range(rows):
-21            for c in range(cols):
-22                if grid[r][c] == 1:
-23                    res = max(res,dfs(r,c))
-24        return res
+10            
+11            area = 1
+12            grid[r][c] = 0
+13            for dr,dc in directions:
+14                area += dfs(r+dr,c+dc)
+15            return area
+16        
+17        for r in range(rows):
+18            for c in range(cols):
+19                if grid[r][c] == 1:
+20                    res = max(res,dfs(r,c))
+21        return res
