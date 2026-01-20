64eb0cb8405527b3534ecd73d959307abcebc205
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-20 03:50:47 -0500

    File updated at 1/20/2026, 3:50:47 AM

--- 213-HouseRobberII/213-HouseRobberII.py
+++ 213-HouseRobberII/213-HouseRobberII.py
@@ -1,13 +1,13 @@
-# Last updated: 11/13/2025, 4:38:09 AM
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        return max(nums[0],self.rob2(nums[1:]),self.rob2(nums[:-1]))
-        
-        
-    def rob2(self,nums):
-        rob1,rob2 = 0,0
-        for i in range(len(nums)):
-            maxrob = max(rob1 + nums[i],rob2)
-            rob1 = rob2
-            rob2 = maxrob
-        return rob2
+# Last updated: 1/20/2026, 3:50:47 AM
+1class Solution:
+2    def rob(self, nums: List[int]) -> int:
+3        return max(nums[0],self.rob2(nums[1:]),self.rob2(nums[:-1]))
+4    
+5
+6    def rob2(self,nums):
+7        prev2,prev1 = 0,0
+8        for i in range(len(nums)):
+9            maxrob = max(prev2 + nums[i],prev1)
+10            prev2 = prev1
+11            prev1 = maxrob
+12        return prev1
