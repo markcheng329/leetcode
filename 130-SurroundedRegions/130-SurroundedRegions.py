e0de00916cc2462a3ede218d098839469cf43b17
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-21 04:02:13 -0500

    File updated at 1/21/2026, 4:02:13 AM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -1,32 +1,30 @@
-# Last updated: 1/21/2026, 4:01:45 AM
+# Last updated: 1/21/2026, 4:02:13 AM
 1class Solution:
 2    def solve(self, board: List[List[str]]) -> None:
 3        """
 4        Do not return anything, modify board in-place instead.
 5        """
 6        rows,cols = len(board),len(board[0])
-7        directions = [1,0],[-1,0],[0,1],[0,-1]
-8
-9        def dfs(r,c):
-10            if r not in range(rows) or c not in range(cols) or board[r][c] != "O":
-11                return
-12            
-13            board[r][c] = "#"
-14            for dr,dc in directions:
-15                dfs(r+dr,c+dc)
-16        
-17        for r in range(rows):
-18            dfs(r,0)
-19            dfs(r,cols-1)
-20        
-21        for c in range(cols):
-22            dfs(0,c)
-23            dfs(rows-1,c)
-24        
-25        for r in range(rows):
-26            for c in range(cols):
-27                if board[r][c] == "O":
-28                    board[r][c] = "X"
-29                elif board[r][c] == "#":
-30                    board[r][c] = "O"
-31
+7
+8        def dfs(r,c):
+9            if r not in range(rows) or c not in range(cols) or board[r][c] != "O":
+10                return
+11            
+12            board[r][c] = "#"
+13            for dr,dc in [1,0],[-1,0],[0,1],[0,-1]:
+14                dfs(r+dr,c+dc)
+15        
+16        for r in range(rows):
+17            dfs(r,0)
+18            dfs(r,cols-1)
+19        
+20        for c in range(cols):
+21            dfs(0,c)
+22            dfs(rows-1,c)
+23        
+24        for r in range(rows):
+25            for c in range(cols):
+26                if board[r][c] == "O":
+27                    board[r][c] = "X"
+28                elif board[r][c] == "#":
+29                    board[r][c] = "O"
