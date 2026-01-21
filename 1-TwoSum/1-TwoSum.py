4a154081717a9963fc68a9d5e1adb90c3d323c2a
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-21 05:16:46 -0500

    File updated at 1/21/2026, 5:16:46 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,11 +1,11 @@
-# Last updated: 12/26/2025, 11:15:33 PM
+# Last updated: 1/21/2026, 5:16:46 AM
 1class Solution:
 2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        seen = {}
+3        hashmap = {}
 4
 5        for i in range(len(nums)):
 6            diff = target - nums[i]
-7            if diff in seen:
-8                return [seen[diff],i]
-9            seen[nums[i]] = i
-10
+7            if diff in hashmap:
+8                return [hashmap[diff],i]
+9            hashmap[nums[i]] = i  
+10        
