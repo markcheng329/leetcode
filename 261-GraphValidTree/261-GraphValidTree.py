28a6f59af0c732da1e9f6d00040dbd24f9f6dec8
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-21 04:16:17 -0500

    File updated at 1/21/2026, 4:16:17 AM

--- 261-GraphValidTree/261-GraphValidTree.py
+++ 261-GraphValidTree/261-GraphValidTree.py
@@ -1,29 +1,32 @@
-# Last updated: 1/20/2026, 2:57:41 AM
+# Last updated: 1/21/2026, 4:16:17 AM
 1class Solution:
 2    def validTree(self, n: int, edges: List[List[int]]) -> bool:
-3        if len(edges) > n-1:
-4            return False
-5        
-6        adj = [[] for i in range(n)]
-7
-8        for u,v in edges:
-9            adj[u].append(v)
-10            adj[v].append(u)
-11        
-12        visit = set()
-13        
-14        def dfs(node,parent):
-15            if node in visit:
-16                return False
-17            
-18            visit.add(node)
-19            for nei in adj[node]:
-20                if nei == parent:
-21                    continue
-22                
+3
+4        if len(edges) > n-1:
+5            return False
+6
+7        adj = [[]for i in range(n)]
+8
+9        for u,v in edges:
+10            adj[u].append(v)
+11            adj[v].append(u)
+12        
+13        visited = set()
+14
+15        def dfs(node,parent):
+16            if node in visited:
+17                return False
+18            
+19            visited.add(node)
+20            for nei in adj[node]:
+21                if nei == parent:
+22                    continue
 23                if not dfs(nei,node):
 24                    return False
 25            return True
 26        
-27        return True if dfs(0,-1) and len(visit) == n else False
-28            
+27        return True if dfs(0,-1) and len(visited) == n else False
+28
+29
+30
+31
