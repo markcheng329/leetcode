1b319eebc002366feb8373f86b11116bc647e1fe
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-21 05:09:19 -0500

    File updated at 1/21/2026, 5:09:19 AM

--- 198-HouseRobber/198-HouseRobber.py
+++ 198-HouseRobber/198-HouseRobber.py
@@ -1,4 +1,4 @@
-# Last updated: 1/20/2026, 3:47:55 AM
+# Last updated: 1/21/2026, 5:09:19 AM
 1class Solution:
 2    def rob(self, nums: List[int]) -> int:
 3        prev2,prev1 = 0,0
@@ -6,4 +6,5 @@
 5            maxrob = max(prev2+nums[i],prev1)
 6            prev2 = prev1
 7            prev1 = maxrob
-8        return prev1
+8        return maxrob
+9
