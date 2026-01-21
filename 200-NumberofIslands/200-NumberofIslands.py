d732328e9c63e6fb10089d803af0c3e0bb0192cd
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-21 05:41:58 -0500

    File updated at 1/21/2026, 5:41:57 AM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,21 +1,28 @@
-# Last updated: 1/21/2026, 5:38:30 AM
+# Last updated: 1/21/2026, 5:41:57 AM
 1class Solution:
 2    def numIslands(self, grid: List[List[str]]) -> int:
 3        rows,cols = len(grid),len(grid[0])
 4        directions = [1,0],[-1,0],[0,1],[0,-1]
 5        res = 0
-6
-7        def dfs(r,c):
-8            if r not in range(rows) or c not in range(cols) or grid[r][c] == "0":
-9                return
-10            
-11            grid[r][c] = "0"
-12            for dr,dc in directions:
-13                dfs(r+dr,c+dc)
-14        
-15        for r in range(rows):
-16            for c in range(cols):
-17                if grid[r][c] == "1":
-18                    dfs(r,c)
-19                    res +=1
-20        return res
+6        q = deque()
+7
+8        def bfs(r,c):
+9            grid[r][c] = "0"
+10            q.append((r,c))
+11
+12            while q:
+13                r,c = q.popleft()
+14                for dr,dc in directions:
+15                    nr,nc = r+dr,c+dc
+16                    if nr not in range(rows) or nc not in range(cols) or grid[nr][nc] == "0":
+17                        continue
+18                    grid[nr][nc] = "0"
+19                    q.append((nr,nc))
+20            
+21        
+22        for r in range(rows):
+23            for c in range(cols):
+24                if grid[r][c] == "1":
+25                    bfs(r,c)
+26                    res +=1
+27        return res
