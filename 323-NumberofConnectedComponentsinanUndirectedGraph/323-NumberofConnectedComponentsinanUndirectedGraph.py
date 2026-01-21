deda6aaed91171e96b585b0781e40b048cb4371b
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-21 04:44:32 -0500

    File updated at 1/21/2026, 4:44:32 AM

--- 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
+++ 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
@@ -1,25 +1,26 @@
-# Last updated: 1/20/2026, 3:01:24 AM
+# Last updated: 1/21/2026, 4:44:32 AM
 1class Solution:
 2    def countComponents(self, n: int, edges: List[List[int]]) -> int:
-3        adj = [[] for i in range(n)]
+3        adj = [[]for i in range(n)]
 4
 5        for u,v in edges:
 6            adj[u].append(v)
 7            adj[v].append(u)
-8        res = 0
-9        
-10        visit = [False] * n
-11        
-12        def dfs(node):
-13            for nei in adj[node]:
-14                if visit[nei] == False:
-15                    visit[nei] = True
-16                    dfs(nei)
-17
-18        for node in range(n):
-19            if visit[node] == False:
-20                visit[node] = True
-21                dfs(node)
-22                res +=1
-23        return res
-24
+8        
+9        res = 0
+10
+11        visit = [False] * n
+12
+13        def dfs(node):
+14            for nei in adj[node]:
+15                if visit[nei] == False:
+16                    visit[nei] = True
+17                    dfs(nei)
+18                
+19        for node in range(n):
+20            if visit[node] == False:
+21                visit[node] = True
+22                dfs(node)
+23                res +=1
+24        return res
+25
