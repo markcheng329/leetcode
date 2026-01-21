2b5a99c4ca2c16a88b0cde019fdbadf0301c7353
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-21 05:02:47 -0500

    File updated at 1/21/2026, 5:02:47 AM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,9 +1,10 @@
-# Last updated: 1/20/2026, 3:08:33 AM
+# Last updated: 1/21/2026, 5:02:47 AM
 1class Solution:
 2    def climbStairs(self, n: int) -> int:
 3        cur,prev = 1,1
-4        for i in range(n-1):
-5            temp = cur
-6            cur = cur+prev
-7            prev = temp
-8        return cur
+4
+5        for i in range(n-1):
+6            temp = cur
+7            cur = cur+ prev
+8            prev = temp
+9        return cur
