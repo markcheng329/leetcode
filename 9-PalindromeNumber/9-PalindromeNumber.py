a1ae9162b7a958a46de8356cbeeb2b3a66c643c5
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-22 05:50:10 -0500

    File updated at 1/22/2026, 5:50:10 AM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,15 +1,16 @@
-# Last updated: 1/22/2026, 5:45:34 AM
+# Last updated: 1/22/2026, 5:50:10 AM
 1class Solution:
 2    def isPalindrome(self, x: int) -> bool:
 3        if x < 0:
 4            return False
-5
+5        
 6        if x > 0 and x % 10 == 0:
 7            return False
-8
+8        
 9        rev = 0
 10        while x > rev:
-11            rev = rev * 10 + x % 10
-12            x //= 10
-13
-14        return x == rev or x == rev // 10
+11            rev = rev * 10 + x%10
+12            x = x//10
+13        
+14        return True if x == rev or x == rev//10 else False
+15
