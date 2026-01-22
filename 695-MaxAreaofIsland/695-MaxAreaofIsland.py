e355c7de1479437386feaf73c7a284c8c5c08d9d
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-22 04:36:44 -0500

    File updated at 1/22/2026, 4:36:44 AM

--- 695-MaxAreaofIsland/695-MaxAreaofIsland.py
+++ 695-MaxAreaofIsland/695-MaxAreaofIsland.py
@@ -1,22 +1,28 @@
-# Last updated: 1/22/2026, 4:35:57 AM
+# Last updated: 1/22/2026, 4:36:44 AM
 1class Solution:
 2    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 3        rows,cols = len(grid),len(grid[0])
-4        directions = [1,0],[-1,0],[0,1],[0,-1]
-5        res = 0
+4        res = 0
+5        directions = [1,0],[-1,0],[0,1],[0,-1]
 6
-7        def dfs(r,c):
-8            if r not in range(rows) or c not in range(cols) or grid[r][c] == 0:
-9                return 0
-10
+7        def bfs(r,c):
+8            q = deque()
+9            grid[r][c] = 0
+10            q.append((r,c))
 11            area = 1
-12            grid[r][c] = 0
-13            for dr,dc in directions:
-14                area += dfs(r+dr,c+dc)
-15            return area
-16        
-17        for r in range(rows):
-18            for c in range(cols):
-19                if grid[r][c] == 1:
-20                    res = max(res,dfs(r,c))
-21        return res
+12            while q:
+13                r,c = q.popleft()
+14                for dr,dc in directions:
+15                    nr,nc = r+dr,c+dc
+16                    if nr not in range(rows) or nc not in range(cols) or grid[nr][nc] == 0:
+17                        continue
+18                    grid[nr][nc] = 0
+19                    q.append((nr,nc))
+20                    area +=1
+21            return area
+22
+23        for r in range(rows):
+24            for c in range(cols):
+25                if grid[r][c] == 1:
+26                    res = max(res,bfs(r,c))
+27        return res
