c1a8783a08454f3bfc1e34b6c90948c690d8675e
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-22 05:38:14 -0500

    File updated at 1/22/2026, 5:38:14 AM

--- 7-ReverseInteger/7-ReverseInteger.py
+++ 7-ReverseInteger/7-ReverseInteger.py
@@ -1,4 +1,4 @@
-# Last updated: 1/22/2026, 5:38:00 AM
+# Last updated: 1/22/2026, 5:38:14 AM
 1class Solution:
 2    def reverse(self, x: int) -> int:
 3        intmax = 2**31-1
@@ -17,7 +17,7 @@
 16
 17            if res > intmax // 10 or (res == intmax//10 and digit > 7):
 18                return 0
-19            if res < min_div10 or (res == min_div10 and digit < -8):
+19            if res < int(intmin / 10)  or (res == int(intmin / 10)  and digit < -8):
 20                return 0 
 21            
 22            res = res* 10 + digit
