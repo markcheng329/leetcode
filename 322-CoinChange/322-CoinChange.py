ac971bffdcb5876d20cb0a671c67e12b9e26460b
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-27 03:42:32 -0500

    File updated at 1/27/2026, 3:42:32 AM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,10 +1,11 @@
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount+1]*(amount+1)
-        dp[0] = 0
-
-        for a in range(1,amount +1):
-            for c in coins:
-                if a-c>=0:
-                    dp[a] = min([dp[a],1+dp[a-c]])
-        return dp[amount ] if dp[amount] != amount +1 else -1
+# Last updated: 1/27/2026, 3:42:32 AM
+1class Solution:
+2    def coinChange(self, coins: List[int], amount: int) -> int:
+3        dp = [amount +1] * (amount + 1)
+4        dp[0] = 0
+5
+6        for i in range(1,amount +1):
+7            for c in coins:
+8                if i-c >= 0:
+9                    dp[i] = min(dp[i],dp[i-c]+1)
+10        return dp[amount] if dp[amount] != amount+1 else -1
