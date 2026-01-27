1373b2e9c47170480ec7f3702819ac709d47bb5b
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-27 04:33:52 -0500

    File updated at 1/27/2026, 4:33:52 AM

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.py
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.py
@@ -1,14 +1,14 @@
-# Last updated: 11/12/2025, 4:58:50 AM
-class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        res = nums[0]
-        curMin,curMax = 1,1
-
-        for n in nums:
-            temp = n * curMax
-
-            curMax = max(n * curMax, n * curMin, n)
-            curMin = min(temp, n * curMin, n)
-            res = max(res, curMax)
-        return res
-        
+# Last updated: 1/27/2026, 4:33:52 AM
+1class Solution:
+2    def maxProduct(self, nums: List[int]) -> int:
+3        max_ans = min_ans = ans = nums[0]
+4
+5        for i in range(1,len(nums)):
+6            if nums[i] < 0:
+7                max_ans, min_ans = min_ans,max_ans
+8
+9            max_ans = max(nums[i],max_ans*nums[i])
+10            min_ans = min(nums[i],min_ans*nums[i])
+11
+12            ans = max(ans,max_ans)
+13        return ans
