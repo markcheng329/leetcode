fb969a575c575d92513c0748a450ef42526ef5d1
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-27 01:26:22 -0500

    Create

---
+++ problem-1769495181890/problem-1769495181890.py
@@ -0,0 +1,18 @@
+# Last updated: 1/27/2026, 1:26:21 AM
+1class Solution:
+2    def countSubstrings(self, s: str) -> int:
+3        res = 0
+4
+5        for i in range(len(s)):
+6            l,r = i,i
+7            while l >=0 and r < len(s) and s[l] == s[r]:
+8                res +=1
+9                l -=1
+10                r +=1
+11            
+12            l,r = i,i+1
+13            while l >= 0 and r < len(s) and s[l] == s[r]:
+14                res +=1
+15                l -=1
+16                r+=1
+17        return res
