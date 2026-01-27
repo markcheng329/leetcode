ef8451c2b74b5278e4e2003dc4b9c7829472757f
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-27 04:49:33 -0500

    File updated at 1/27/2026, 4:49:33 AM

--- 139-WordBreak/139-WordBreak.py
+++ 139-WordBreak/139-WordBreak.py
@@ -1,22 +1,23 @@
-# Last updated: 1/27/2026, 4:37:34 AM
+# Last updated: 1/27/2026, 4:49:33 AM
 1class Solution:
 2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 3        words = set(wordDict)
 4        n = len(s)
-5
-6        q = deque([0])      # 队列里存：当前能到达的下标
-7        visited = set()    # 记录已经处理过的起点
+5        lens = set(len(w) for w in words)
+6        q = deque([0])
+7        visit = set()
 8
 9        while q:
 10            start = q.popleft()
-11            if start in visited:
+11            if start in visit:
 12                continue
-13            visited.add(start)
+13            visit.add(start)
 14
-15            for end in range(start + 1, n + 1):
-16                if s[start:end] in words:
-17                    if end == n:
-18                        return True
-19                    q.append(end)
-20
-21        return False
+15            for l in lens:
+16                end = start + l
+17                if end <= n and s[start:end] in words:
+18                    if end == n:
+19                        return True
+20                    
+21                    q.append(end)
+22        return False
