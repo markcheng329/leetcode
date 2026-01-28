b4fded6f4d710ace81e93bc99a26399cf65f619f
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-28 11:38:31 -0500

    File updated at 1/28/2026, 11:38:31 AM

--- 139-WordBreak/139-WordBreak.py
+++ 139-WordBreak/139-WordBreak.py
@@ -1,27 +1,22 @@
-# Last updated: 1/28/2026, 11:19:06 AM
+# Last updated: 1/28/2026, 11:38:31 AM
 1class Solution:
 2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 3        words = set(wordDict)
-4        n = len(s)
-5        lens = set(len(w) for w in words)
-6        q = deque([0])
-7        visit = set()
+4        lens = set(len(w) for w in words)
+5        q = deque([0])
+6        visit =set()
+7        n = len(s)
 8
-9
-10        while q:
-11            start = q.popleft()
-12            if start in visit:
-13                continue
-14            visit.add(start)
-15
-16            for l in lens:
-17                end = start + l
-18                if end <= n and s[start:end] in words:
-19                    if end == n:
-20                        return True
-21                    q.append(end)
-22        return False
-23
-24
-25
-26
+9        while q:
+10            start = q.popleft()
+11            if start in visit:
+12                continue
+13            visit.add(start)
+14
+15            for l in lens:
+16                end = start + l
+17                if end <= n and s[start:end] in words:
+18                    if end == n:
+19                        return True
+20                    q.append(end)
+21        return False
