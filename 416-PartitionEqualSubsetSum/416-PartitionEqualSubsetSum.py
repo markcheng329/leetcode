c6c1f589e6d0d756ff608974139d726c1711bea6
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-28 11:55:33 -0500

    File updated at 1/28/2026, 11:55:32 AM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
@@ -1,19 +1,17 @@
-# Last updated: 1/27/2026, 5:44:45 AM
+# Last updated: 1/28/2026, 11:55:32 AM
 1class Solution:
 2    def canPartition(self, nums: List[int]) -> bool:
 3        total = sum(nums)
-4
-5        if total % 2 == 1:
-6            return False
-7        
-8        target = total //2
-9
-10        dp = [False] * (target+1)
-11
-12        dp[0] = True
-13
-14        for i in range(len(nums)):
-15            for t in range(target,nums[i]-1,-1):
-16                dp[t] = dp[t] or dp[t-nums[i]]
-17        
-18        return dp[target]
+4        if total % 2 == 1:
+5            return False
+6        
+7        target = total //2
+8        dp = [False] * (target+1)
+9        dp[0] = True
+10        
+11        for i in range(len(nums)):
+12            for t in range(target,nums[i]-1,-1):
+13                dp[t] = dp[t] or dp[t-nums[i]]
+14        
+15        return dp[target]
+16
