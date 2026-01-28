746b31a5a0adcb11af42ee1cfadcfd8c07be97e5
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-28 10:02:23 -0500

    File updated at 1/28/2026, 10:02:23 AM

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.py
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.py
@@ -1,20 +1,18 @@
-# Last updated: 11/13/2025, 4:49:40 AM
-class Solution:
-    def countSubstrings(self, s: str) -> int:
-        res = 0
-
-        for i in range(len(s)):
-            l,r = i,i
-            while l >= 0 and r< len(s) and s[l] == s[r]:
-                res +=1
-                l -=1
-                r +=1
-            
-            l,r = i,i+1
-            while l >= 0 and r< len(s) and s[l] == s[r]:
-                res +=1
-                l -=1
-                r +=1
-            
-        return res
-                
+# Last updated: 1/28/2026, 10:02:23 AM
+1class Solution:
+2    def countSubstrings(self, s: str) -> int:
+3        res = 0
+4
+5        for i in range(len(s)):
+6            l,r = i,i
+7            while l >= 0 and r< len(s) and s[l] == s[r]:
+8                res +=1
+9                l-=1
+10                r+=1
+11            
+12            l,r = i,i+1
+13            while l >= 0 and r < len(s) and s[l] == s[r]:
+14                res +=1
+15                l -=1
+16                r+=1
+17        return res
