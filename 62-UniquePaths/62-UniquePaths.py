42c2c183bac7f82fd830b5b624b9001a3d67f497
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-28 18:47:50 -0500

    File updated at 1/28/2026, 6:47:50 PM

--- 62-UniquePaths/62-UniquePaths.py
+++ 62-UniquePaths/62-UniquePaths.py
@@ -1,7 +1,10 @@
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        dp = [1] * n
-        for i in range(m-2,-1,-1):
-            for j in range(n-2,-1,-1):
-                dp[j]+=dp[j+1]
-        return dp[0]
+# Last updated: 1/28/2026, 6:47:50 PM
+1class Solution:
+2    def uniquePaths(self, m: int, n: int) -> int:
+3        dp = [1] * n
+4        for i in range(m-2,-1,-1):
+5            for j in range(n-2,-1,-1):
+6                dp[j]+=dp[j+1]
+7        return dp[0]
+8
+9        
