c2f7eced2f7055f99e9310149449d7f9dae2cb75
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-28 10:39:52 -0500

    File updated at 1/28/2026, 10:39:51 AM

--- 91-DecodeWays/91-DecodeWays.py
+++ 91-DecodeWays/91-DecodeWays.py
@@ -1,4 +1,4 @@
-# Last updated: 1/28/2026, 10:08:43 AM
+# Last updated: 1/28/2026, 10:39:51 AM
 1class Solution:
 2    def numDecodings(self, s: str) -> int:
 3        n = len(s)
@@ -11,12 +11,12 @@
 10
 11        for i in range(2,n+1):
 12            cur = 0
-13            if s[i-1] != "0":
-14                cur += dp1
-15
-16            if 10 <= int(s[i-2:i]) <= 26:
-17                cur += dp0
-18
-19            dp0,dp1 = dp1,cur
-20
-21        return dp1 
+13
+14            if s[i-1] != "0":
+15                cur += dp1
+16            
+17            if 10 <= int(s[i-2:i]) <= 26:
+18                cur += dp0
+19            
+20            dp0,dp1 = dp1,cur
+21        return dp1
