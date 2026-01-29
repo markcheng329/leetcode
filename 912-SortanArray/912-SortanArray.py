54f18bfdd2167512b3ce7f756608e6800135bc3a
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-28 23:29:33 -0500

    File updated at 1/28/2026, 11:29:33 PM

--- 912-SortanArray/912-SortanArray.py
+++ 912-SortanArray/912-SortanArray.py
@@ -1,36 +1,35 @@
-# Last updated: 12/10/2025, 11:53:18 PM
+# Last updated: 1/28/2026, 11:29:33 PM
 1class Solution:
 2    def sortArray(self, nums: List[int]) -> List[int]:
-3        if len (nums) <= 1:
+3        if len(nums) <= 1:
 4            return nums
 5        
-6        mid = (len(nums)) //2
+6        mid = (len(nums))//2
 7
 8        left = self.sortArray(nums[:mid])
 9        right = self.sortArray(nums[mid:])
-10    
+10
 11        return self.merge(left,right)
-12
-13    def merge(self,left,right):
-14        i = j = 0
-15        res = []
-16
-17        while i < len(left) and j < len(right):
-18            if left[i] < right[j]:
-19                res.append(left[i])
-20                i +=1
-21            else:
-22                res.append(right[j])
-23                j +=1
-24        
-25        while i < len(left):
-26            res.append(left[i])
-27            i +=1
-28
-29        while j < len(right):
-30            res.append(right[j])
-31            j +=1
-32
+12    
+13
+14    def merge(self,left,right):
+15        i,j = 0,0
+16        res = []
+17
+18        while i < len(left) and j < len(right):
+19            if left[i] < right[j]:
+20                res.append(left[i])
+21                i +=1
+22            else:
+23                res.append(right[j])
+24                j +=1
+25
+26        while i < len(left):
+27            res.append(left[i])
+28            i +=1
+29        
+30        while j < len(right):
+31            res.append(right[j])
+32            j +=1
 33        return res
 34
-35
