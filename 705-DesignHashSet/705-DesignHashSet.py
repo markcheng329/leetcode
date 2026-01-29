31e9e0bb3b3b7d98ea401b2471f26929ac3d030b
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-01-28 22:50:07 -0500

    File updated at 1/28/2026, 10:50:07 PM

--- 705-DesignHashSet/705-DesignHashSet.py
+++ 705-DesignHashSet/705-DesignHashSet.py
@@ -1,24 +1,25 @@
-# Last updated: 12/10/2025, 11:03:51 PM
+# Last updated: 1/28/2026, 10:50:07 PM
 1class MyHashSet:
 2
 3    def __init__(self):
-4        self.data = [False] * (10**6+1)    
-5
-6    def add(self, key: int) -> None:
-7        self.data[key] = True
-8
+4        self.hashset = [False] * (10**6+1)
+5        
+6
+7    def add(self, key: int) -> None:
+8        self.hashset[key] = True
 9        
-10    def remove(self, key: int) -> None:
-11        self.data[key] = False
-12        
-13
-14    def contains(self, key: int) -> bool:
-15        return self.data[key]
-16        
-17
+10
+11    def remove(self, key: int) -> None:
+12        self.hashset[key] = False
+13        
+14
+15    def contains(self, key: int) -> bool:
+16        return self.hashset[key]
+17        
 18
-19# Your MyHashSet object will be instantiated and called as such:
-20# obj = MyHashSet()
-21# obj.add(key)
-22# obj.remove(key)
-23# param_3 = obj.contains(key)
+19
+20# Your MyHashSet object will be instantiated and called as such:
+21# obj = MyHashSet()
+22# obj.add(key)
+23# obj.remove(key)
+24# param_3 = obj.contains(key)
