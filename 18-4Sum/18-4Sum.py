bf47f5a38e7689e1c4438a6be92f9c8a16a8a60c
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-02-02 20:02:53 -0500

    File updated at 2/2/2026, 8:02:53 PM

--- 18-4Sum/18-4Sum.py
+++ 18-4Sum/18-4Sum.py
@@ -1,4 +1,4 @@
-# Last updated: 12/9/2025, 12:54:40 AM
+# Last updated: 2/2/2026, 8:02:53 PM
 1class Solution:
 2    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
 3        nums.sort()
@@ -10,21 +10,22 @@
 9            for j in range(i+1,len(nums)):
 10                if j > i+1 and nums[j] == nums[j-1]:
 11                    continue
-12                    
+12                
 13                l, r = j+1,len(nums)-1
 14
-15                while l < r:
+15                while l < r :
 16                    total = nums[i] + nums[j] + nums[l] + nums[r]
-17                    if total > target:
-18                        r -=1
-19                    elif total < target:
-20                        l +=1
-21                    else:
-22                        res.append([nums[i],nums[j],nums[l],nums[r]])
-23                        l +=1
-24                        r-=1
-25                        while l < r and nums[l] == nums[l-1]:
-26                            l +=1
-27                        while l < r and nums[r] == nums[r+1]:
-28                            r -=1
-29        return res
+17
+18                    if total > target:
+19                        r -=1
+20                    elif total < target:
+21                        l +=1
+22                    else:
+23                        res.append([nums[i],nums[j],nums[l],nums[r]])
+24                        l +=1
+25                        r -=1
+26                        while l < r and nums[l] == nums[l-1]:
+27                            l +=1
+28                        while l < r and nums[r] == nums[r+1]:
+29                            r -=1
+30        return res
