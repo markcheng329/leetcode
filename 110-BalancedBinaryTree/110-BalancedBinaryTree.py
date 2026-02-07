de542c857197ecd34fa7dc8171ba1b1bae1929ba
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-02-07 15:36:16 -0500

    File updated at 2/7/2026, 3:36:16 PM

--- 110-BalancedBinaryTree/110-BalancedBinaryTree.py
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 2/7/2026, 3:30:58 PM
+# Last updated: 2/7/2026, 3:36:16 PM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -7,21 +7,22 @@
 6#         self.right = right
 7class Solution:
 8    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-9        def dfs(node: Optional[TreeNode]) -> int:
+9        def dfs(node):
 10            if not node:
-11                return 0  # 空树高度为 0
-12
+11                return 0
+12            
 13            lh = dfs(node.left)
 14            if lh == -1:
-15                return -1  # 左子树已不平衡，直接剪枝
-16
+15                return -1
+16            
 17            rh = dfs(node.right)
 18            if rh == -1:
-19                return -1  # 右子树已不平衡，直接剪枝
-20
-21            if abs(lh - rh) > 1:
-22                return -1  # 当前节点不平衡
-23
-24            return 1 + max(lh, rh)  # 返回当前高度
-25
-26        return dfs(root) != -1
+19                return -1
+20            
+21            if abs(rh-lh) > 1:
+22                return -1
+23            
+24            return 1+max(rh,lh)
+25        return dfs(root) != -1
+26            
+27
