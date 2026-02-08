21570c40695035404152a6b4f2a70b131f3063ec
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-02-07 21:00:39 -0500

    File updated at 2/7/2026, 9:00:39 PM

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
@@ -1,17 +1,17 @@
-# Last updated: 11/12/2025, 4:59:05 AM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def valid(node,left,right):
-            if not node:
-                return True
-            if not (left < node.val < right):
-                return False
-            return valid(node.left,left,node.val) and valid(node.right,node.val,right)
-        return valid(root,float("-inf"),float("inf"))
-        
+# Last updated: 2/7/2026, 9:00:39 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+9        def dfs(node, low, high):
+10            if not node:
+11                return True
+12            if not (low < node.val < high):
+13                return False
+14            return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+15
+16        return dfs(root, float("-inf"), float("inf"))
