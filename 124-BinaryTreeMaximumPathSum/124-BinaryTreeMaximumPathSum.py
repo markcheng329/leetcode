94b708704dd58c1e85306365363f0d7563b2f6e7
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-02-07 21:50:57 -0500

    File updated at 2/7/2026, 9:50:57 PM

--- 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
@@ -1,27 +1,24 @@
-# Last updated: 11/12/2025, 4:58:59 AM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        res = [root.val]
-        
-        def dfs(root):
-            if not root:
-                return 0
-
-            leftMax = dfs(root.left)
-            rightMax = dfs(root.right)
-            leftMax = max(leftMax,0)
-            rightMax = max(rightMax,0)
-
-            res[0] = max(res[0],root.val+leftMax+rightMax)
-
-            return root.val + max(leftMax,rightMax)
-        
-        dfs(root)
-        return res[0]
-        
+# Last updated: 2/7/2026, 9:50:57 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+9        best = float("-inf")
+10
+11        def dfs(node):
+12            nonlocal best
+13            if not node:
+14                return 0
+15            
+16            left = max(0,dfs(node.left))
+17            right = max(0,dfs(node.right))
+18
+19            best = max(best,node.val+left+right)
+20            return node.val + max(left,right)
+21        dfs(root)
+22        return best
+23
