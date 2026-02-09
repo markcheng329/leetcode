58226279767208017016dd3b4beceb9ff1be2c50
Mark Cheng <115590620+markcheng329@users.noreply.github.com>
2026-02-09 17:20:22 -0500

    Create 518-CoinChangeII

---
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -0,0 +1,11 @@
+# Last updated: 2/9/2026, 5:20:21 PM
+1class Solution:
+2    def change(self, amount: int, coins: List[int]) -> int:
+3        dp = [0] * (amount + 1)
+4        dp[0] = 1
+5
+6        for c in coins:
+7            for x in range(c, amount + 1):
+8                dp[x] += dp[x - c]
+9
+10        return dp[amount]
